7401a9d674ffe774674b0f642b70118411cb52df
Dariusz Kobuszewski <dariusz.kobuszewski@doskomp.lodz.pl>
2014-12-15 22:40:50 +0100

    Proxy-services, tried to make songza-api-proxy working through http.request.

--- server/app.js
+++ server/app.js
@@ -19,7 +19,8 @@ if(config.seedDB) { require('./config/seed'); }
 
 // Setup server
 var app = express();
-var server = require('http').createServer(app);
+var http = require('http');
+var server = http.createServer(app);
 var socketio = require('socket.io')(server, {
   serveClient: (config.env === 'production') ? false : true,
   path: '/socket.io-client'
@@ -32,6 +33,102 @@ require('./routes')(app);
 server.listen(config.port, config.ip, function () {
   console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
 });
+
+// DK: Proxy servers:
+var httpProxy = require('http-proxy');
+
+var proxy = httpProxy.createProxyServer({xfwd : true, changeOrigin: true }),
+  url = require('url');
+
+var proxy2 = httpProxy.createProxyServer({xfwd : true, changeOrigin: true });
+// start proxy server:
+http.createServer(function(req, res) {
+ // console.log('Proxy-server listening on %d, in %s mode', config.port, app.get('env'));
+  var proxyHost, pattern;
+
+   //console.log(req.url);
+  // #1 songza-api (straight-forward to songza.com)
+  pattern = /^\/songza-api\/(.*)/;
+  if (pattern.exec(req.url)) {
+    proxyHost = 'http://songza.com/api/1/' + req.url.match(pattern)[1];
+    //console.log("9001 proxy server requested, proxyHost: " + proxyHost);
+
+    // req.headers['Access-Control-Allow-Origin'] = req.headers['origin'];
+    // req.headers['Access-Control-Allow-Credentials'] = "true";
+    res.setHeader('Access-Control-Allow-Origin', req.headers['origin']);
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+    // console.log(req.headers);
+    proxy.web(req, res, { target: proxyHost });
+  }
+
+  // #2 songza-api-proxy (throught the heroku transparent proxy server located in USA :) )
+  pattern = /\/songza-api-proxy\/(.*)/;
+  if (pattern.exec(req.url)) {
+    proxyHost = 'http://transparent-proxy.herokuapp.com/proxy.php?' /*+ req.url.match(pattern)[2] + '&*/ + '__dest_url=/' + req.url.match(pattern)[1];
+    var options = {
+      host: 'transparent-proxy.herokuapp.com',
+      path: '/proxy.php?' + '__dest_url=/' + req.url.match(pattern)[1],
+    };
+
+    var callback = function(response) {
+      var str = '';
+      //another chunk of data has been recieved, so append it to `str`
+      response.on('data', function (chunk) {
+        str += chunk;
+      });
+      //the whole response has been recieved, so we just print it out here
+      response.on('end', function () {
+        console.log(str);
+        res.write(str);
+      });
+      };
+    http.request(options, callback).end();
+    //console.log("9001 transparent-proxy server requested, proxytargetHost: " + proxyHost);
+//
+    //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:9001');
+    //res.setHeader('Access-Control-Allow-Credentials', 'true');
+    ////console.log(res);
+    //proxy2.web(req, res, { target: proxyHost, changeOrigin: true, hostRewrite: true});
+  }
+  // res.end();
+
+}).listen(9001, config.ip);
+
+proxy.on('proxyRes', function (proxyRes, req, res) {
+  console.log('RAW Response from the target', JSON.stringify(proxyRes.headers, true, 2));
+});
+// httpProxy.createServer(function (req, res, proxy) {
+//   var proxyHost;
+//   var pattern = /^\/songza-api\/(.*)/g;
+//   debugger;
+//   console.log(req.url);
+//   if (pattern.exec(req.url)) {
+//     proxyHost = 'http://songza.com/api/1/' + req.url.match(pattern)[0];
+//     console.log("9001 proxy server requested, proxyHost: ");
+//     console.log(proxyHost);
+//     //req.headers["X-CUSTOM-API-KEY"] = 'my-api-key';
+//     proxy.proxyRequest(req, res, {
+//       target : proxyHost
+//     });
+//   }
+// }).listen(9001);
+
+console.log("PROXY LISTENING ON 9001");
+
+/*
+httpProxy.createProxyServer({target:'http://songza.com/api/1'}).listen(8000);
+
+app.all("/songza-api/*", function(req, res) {
+  console.log("old request url " + req.url)
+  req.url = '/' + req.url.split('/').slice(2).join('/'); // remove the '/api' part
+  console.log("new request url " + req.url)
+  httpProxy.proxyRequest(req, res, {
+    host: "other_domain.com",
+    port: 3000
+  });
+});
+*/
 
 // Expose app
 exports = module.exports = app;
