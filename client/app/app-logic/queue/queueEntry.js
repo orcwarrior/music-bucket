1648041aef3b58c5f7ea9d84a35a7e0b048ae637
orcwarrior <orcwarrior@o2.pl>
2015-02-06 21:20:52 +0100

    Queue entry: Logs + enabled re-buffering (even it's probalby blocked by SM2 anyway)

--- client/app/app-logic/queue/queueEntry.js
+++ client/app/app-logic/queue/queueEntry.js
@@ -4,7 +4,7 @@
 
 (function () {
   angular.module('musicBucketEngine')
-    .factory('queueEntry', function (SMSoundConverter) {
+    .factory('queueEntry', function (SMSoundConverter, $log) {
                return {
                  constructor: function (song, SMSound) {
                    this.song = song;
@@ -12,21 +12,26 @@
                    this.SMSound = SMSound; // instance of soundmanager sound
 
                    this.buffer = function () {
-                     if (this.buffered == true) return;
+                     $log.info('queueEntry: buffer on: ' + this.song.shared.id + ' - ' + this.song.shared.getSongDescription() + ' called.');
+                     //if (this.buffered == true) return;
 
                      if (this.SMSound != null) {
+                       $log.info('queueEntry: ..already has SMSound, buffer it!');
                        this.SMSound.load();
                        this.buffered = true;
                      }
                      // get entry
                      else {
+                       $log.info('queueEntry: ..create and buffer SMSound!');
                        this.SMSound = SMSoundConverter.createAndBufferFromSong(this.song);
                        this.buffered = true;
                      }
                      return this.buffered;
                    }
                    this.play = function () {
+                     $log.info('queueEntry: play on: '+this.song.shared.id + ' - ' + this.song.shared.getSongDescription() + ' called.');
                      if (this.SMSound == undefined) {
+                       $log.info('queueEntry: play: song has to be created and buffered.');
                        this.SMSound = SMSoundConverter.createAndBufferFromSong(this.song);
                      }
                        this.SMSound.play();
