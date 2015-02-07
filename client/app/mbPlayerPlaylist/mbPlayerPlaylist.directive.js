c77a91726ca6858000ff94632ddefacbc081822f
orcwarrior <orcwarrior@o2.pl>
2015-02-08 00:53:56 +0100

    Angular directive for player playlist (mbPlayerPlaylist) with responsive height (little less than device window height) or till the buttom of window for desktop devices.

---
+++ client/app/mbPlayerPlaylist/mbPlayerPlaylist.directive.js
@@ -0,0 +1,78 @@
+'use strict';
+
+angular.module('musicBucketApp')
+  .directive('mbPlayerPlaylist', function ($document, $window) {
+    return  {
+      templateUrl: 'app/mbPlayerPlaylist/mbPlayerPlaylist.html',
+      restrict: 'EA',
+        scope: {
+        playlist: '=playlist'
+      },
+      link: function (scope, element, attrs) {
+        var NAVBAR_HEIGHT = 50;
+        var TOOLBAR_HEIGHT = 0;       // TODO: In toolbarService store some toolbarHeight value
+        var MAINCONTROLL_HEIGHT = 360; // TODO: From where playerMainControlls Height should be taken???
+        element.css({
+                      display: 'block',
+                     'overflow-y': 'auto'
+                    });
+
+        // Recalculate element height on window resize (responsiveness)
+        angular.element($window).bind('resize', function() {
+          recalculateHeight(event);
+          scope.$apply();
+        });
+        // Recalculate on init too:
+        recalculateHeight(event);
+
+        function recalculateHeight() {
+          var wnd = getWindowDimensions();
+          if (wnd.width <= 760) {
+            /* mobile */
+            element.css({height: wnd.height - ( TOOLBAR_HEIGHT + NAVBAR_HEIGHT + 100) + 'px'});
+          } else {
+            /* desktop */
+            element.css({height: wnd.height - (MAINCONTROLL_HEIGHT + NAVBAR_HEIGHT + TOOLBAR_HEIGHT) + 'px'});
+          }
+
+        }
+
+        function getWindowDimensions () {
+          return { width : getWidth(), height: getHeight() };
+        }
+        // TODO: Move to some service
+        function getWidth() {
+          if (self.innerHeight) {
+            return self.innerWidth;
+          }
+
+          if (document.documentElement && document.documentElement.clientHeight) {
+            return document.documentElement.clientWidth;
+          }
+
+          if (document.body) {
+            return document.body.clientWidth;
+          }
+        }
+
+        function getHeight() {
+          if (self.innerHeight) {
+            return self.innerHeight;
+          }
+
+          if (document.documentElement && document.documentElement.clientHeight) {
+            return document.documentElement.clientHeight;
+          }
+
+          if (document.body) {
+            return document.body.clientHeight;
+          }
+        }
+        /*
+        * window.onresize = function(event) {
+         ...
+         };
+        * */
+      }
+    };
+  });
