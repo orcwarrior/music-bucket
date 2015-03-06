cd664362784a4cd25f7ed95ee8b3872b64d9b028
orcwarrior <orcwarrior@o2.pl>
2015-03-06 14:16:12 +0100

    Playlist: moved to function-object, playlist sample song now will be randomed even after all samples are stored plus they will be pushed into DB on any alter.

--- client/app/app-logic/playlist/playlist.js
+++ client/app/app-logic/playlist/playlist.js
@@ -3,85 +3,86 @@
  */
 (function () {
   angular.module('musicBucketEngine')
-    .factory('playlist', function (playlistLocalStorage, playlistSequencers, playlistCookieFactory, Auth) {
+    .factory('playlist', function (playlistLocalStorage, playlistSequencers, playlistService) {
 
-                 function rewriteBase(base) {
-                   if (!_.isUndefined(base)) {
-                     this.entries = base.entries;
-                     this.songsCount = base.entriesCount;
-                   }
-                 }
-                 function putSongToSampler(playlist, song) {
-                      if(song.shared.albumArt && !song.shared.albumArtAttached && playlist.sampleSongs.length < 5) {
-                        playlist.sampleSongs.push({src: song.shared.albumArt, description: song.shared.getSongDescription()});
-                      };
-                 }
-               return {
-                 constructor: function (base) {
-                   var self = this;
+      function rewriteBase(base) {
+        if (!_.isUndefined(base)) {
+          this.entries = base.entries;
+          this.songsCount = base.entriesCount;
+        }
+      }
 
-                   this.name = '';
-                   this.entries = [];
-                   this.songsCount = 0; // count of all songs
+      function putSongToSampler(playlist, song) {
+        var samplerSize = 6;
+        if (song.metainfos.albumArt && !song.metainfos.albumArtAttached && playlist.sampleSongs.length < samplerSize) {
+          playlist.sampleSongs.push({src: song.metainfos.albumArt, description: song.metainfos.getSongDescription()});
+        } else if (Math.random() > 0.9 && playlist.sampleSongs.length === samplerSize) {
+          playlist.sampleSongs[Math.round(Math.random() * samplerSize)] = {
+            src: song.metainfos.albumArt,
+            description: song.metainfos.getSongDescription()
+          };
+          playlistService.save(playlist);
+        }
+        else {
+          return; // break from storing again in db
+        };
+      }
 
-                   this.playlistSequencer = playlistSequencers['default'];
-                   this.nextPlaylistSequencer = function() {
-                     var playlistSequencersArr = playlistSequencers.toArray();
-                     var thisIdx = playlistSequencersArr.indexOf(this.playlistSequencer);
-                     var nextIdx = ++thisIdx % playlistSequencersArr.length;
-                     this.playlistSequencer = playlistSequencersArr[nextIdx];
-                   }
+      return function playlist (base) {
+          var self = this;
 
-                   this.sampleSongs = [];
-                   this.isAltered = false;
+          this.name = '';
+          this.entries = [];
 
-                   _.extend(this, playlistLocalStorage);
-                   _.extend(this, base);
+          this.playlistSequencer = playlistSequencers['default'];
+          this.nextPlaylistSequencer = function () {
+            var playlistSequencersArr = playlistSequencers.toArray();
+            var thisIdx = playlistSequencersArr.indexOf(this.playlistSequencer);
+            var nextIdx = ++thisIdx % playlistSequencersArr.length;
+            this.playlistSequencer = playlistSequencersArr[nextIdx];
+          };
 
-                   // methods:
-                   this.getNext = function () {
-                     return this.playlistSequencer.getNext(this.entries, this.songsCount, this.getNextCallback); /* an promise*/
-                   }
-                   this.getNextCallback = function(song){
-                     putSongToSampler(self, song);
-                   }
-                   this.addEntry = function (entry) {
-                     this.alter();
-                     if (_.isUndefined(entry.songsCount))
-                       this.songsCount++;
-                     else
-                       this.songsCount += entry.songsCount;
-                     this.entries.push(entry);
-                   }
-                   this.removeEntry = function (entryId) {
-                     this.alter();
-                     this.entries =  _.reject(this.entries, function(entry){
-                       return entry.id == entryId;
-                     });
-                   }
+          this.sampleSongs = [];
+          this.isAltered = false;
 
-                   this.alter = function() {
-                     this.isAltered = true;
-                     this.storeInLocalstorage();
-                   }
-                   this.toCookieModel = function() {
-                     var result = {
-                       name : this.name,
-                       songsCount : this.songsCount,
-                       entries : _.map(this.entries, function(entry) { return entry.toCookieModel();}),
-                       sampleSongs : this.sampleSongs,
-                       playlistSequencer : this.playlistSequencer
-                     }
+          _.extend(this, playlistLocalStorage);
+          _.extend(this, base);
 
-                     return result;
-                   }
-                   return this;
-                 } //eof constructor
-               };
-             })
-    .factory('playlistFuncs', function() {
+          // methods:
+          this.getNext = function () {
+            return this.playlistSequencer.getNext(this.entries, this.songsCount, this.getNextCallback);
+            /* an promise*/
+          };
+          this.getNextCallback = function (song) {
+            putSongToSampler(self, song);
+          };
+          this.addEntry = function (entry) {
+            this.entries.push(entry);
+            this.alter();
+          };
+          this.removeEntry = function (entryId) {
+            this.entries = _.reject(this.entries, function (entry) {
+              return entry.id == entryId;
+            });
+            this.alter();
+          };
 
-
-               });
-}
-)();
+          this.alter = function () {
+            this.isAltered = true;
+            this.sampleSongs.splice(Math.round(this.sampleSongs.length * Math.random()), 1);
+            this.recalculateSongsCount();
+            this.storeInLocalstorage();
+          };
+          this.recalculateSongsCount = function () {
+            this.songsCount = 0;
+            _.each(this.entries, function (entry) {
+              if (!_.isUndefined(entry.songsCount))
+                self.songsCount += entry.songsCount;
+              else
+                self.songsCount++;
+            });
+          };
+          return this;
+      };
+    })
+})();
