2d21d20314a961b04a9942741602006387b0864b
orcwarrior <orcwarrior@o2.pl>
2015-03-06 14:17:45 +0100

    Playlist: moved to function-object, playlist sample song now will be randomed even after all samples are stored plus they will be pushed into DB on any alter. (ADD1)

--- client/app/local/local.controller.js
+++ client/app/local/local.controller.js
@@ -1,20 +1,17 @@
 'use strict';
 
 angular.module('musicBucketApp')
-  .controller('LocalCtrl', function ($scope, $rootScope, songLocal, localEntry, angularPlayer) {
+  .controller('LocalCtrl', function ($scope, $rootScope, song, songCommons, localEntry, angularPlayer) {
 
-                $scope.$watch("droppedFiles",
-                              function(val, oldVal, scope) {
-                                if(_.isUndefined(val)) return;
-                                var songs = [], localSongEntries = [];
-                                $rootScope.playlist = $rootScope.playlist || [];
-                                for(var i=0; i<$scope.droppedFiles.length; i++) {
-                                  songs[i] = new songLocal.constructor($scope.droppedFiles[i]);
-                                  localSongEntries[i] = new localEntry.constructor(songs[i]);
-                                  angularPlayer.addToPlaylist(localSongEntries[i]);
-                                  // dbg
-                                  var file = $scope.droppedFiles[i];
-                                  console.log(file.name.substr(0,file.name.lastIndexOf('.')))
-                                }
-                              });
+    $scope.$watch("droppedFiles",
+      function (val, oldVal, scope) {
+        if (_.isUndefined(val)) return;
+        var songs = [], localSongEntries = [];
+        $rootScope.playlist = $rootScope.playlist || [];
+        for (var i = 0; i < $scope.droppedFiles.length; i++) {
+          songs[i] = new song($scope.droppedFiles[i], songCommons.songType.local);
+          localSongEntries[i] = new localEntry.constructor(songs[i]);
+          angularPlayer.addToPlaylist(localSongEntries[i]);
+        }
+      });
   });
