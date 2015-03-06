9de33260746fc4d574b0d85f1d65504c10abd1bf
orcwarrior <orcwarrior@o2.pl>
2015-03-06 14:12:00 +0100

    Playlist entry object reworked little to moved from plain object to object made of function (with prototyping available).

---
+++ client/app/app-logic/playlist/entry/localEntry.js
@@ -0,0 +1,44 @@
+/**
+ * Created by orcwarrior on 2014-12-16.
+ */
+
+(function () {
+  angular.module('musicBucketEngine')
+    .factory('localEntry', function ($rootScope, $q, entryCommons, songYoutube, SMSoundConverter, youtubeApiHelper) {
+      function commonInit(self) {
+        self.type = entryCommons.entryType.localEntry;
+        self.songsCount = 1;
+        self.playedCount = 0;
+
+        self.init = function () {
+        }
+        self.getNext = function (playlistCallback) {
+          var deferred = $q.defer();
+          // TODO: Move to player (on-play, store reference to currenty playlist-entry)
+          this.playedCount++;
+          deferred.resolve(this.entries[0]);
+          playlistCallback(this.entries[0]);
+          return deferred.promise;
+        }
+        self.updateShortDescription = function () {
+          this.shortDescription = this.entries[0].metainfos.artist + " - " + this.entries[0].metainfos.title;
+        }
+        self.getPlaylistDescription = function () {
+          self.updateShortDescription();
+          return this.shortDescription;
+        }
+      } // commonInit
+
+      return function localEntry(localSong) {
+        var self = this;
+        commonInit(this);
+        if (_.isUndefined(localSong)) return;
+
+        this.entries = [localSong];
+        this.id = this.entries[0].metainfos.id;
+        // Temporary, pre warm-up name:
+        this.shortDescription = this.entries[0].metainfos.title;
+        this.init();
+      };
+    });
+})();
---
+++ client/app/app-logic/playlist/entry/songzaStationEntry.js
@@ -0,0 +1,84 @@
+/**
+ * Created by orcwarrior on 2014-11-24.
+ */
+(function () {
+  angular.module('musicBucketEngine')
+    .factory('songzaStationEntry', function ($rootScope, $q, song, entryCommons, songCommons, songzaApi) {
+      function commonInit(self) {
+        self.type = entryCommons.entryType.songzaStation;
+        self.entries = [];
+        self.playedCount = 0;
+        self.playedIDs = [];
+
+        self.init = function (stationId) {
+          // $rootScope.songza.station.get(stationId)
+          // $http.get('/songza-api/station/'+stationId)
+          songzaApi.station.get(stationId)
+            .then(function (response) {
+              self.station = response.data;
+              self.songsCount = response.data.song_count;
+              self.stationName = response.data.name;
+              self.updateShortDescription();
+            });
+        };
+        // Returns promise of songSongza (based on base song object)
+        function tryGetNewSong(songzaPromise, playlistCallback) {
+          //$rootScope.songza.station.nextSong(self.id)
+          //$http.get('/songza-api/station/'+self.id+'/next')
+          songzaApi.station.next(self.id)
+            .then(function (response) {
+              response = response.data; // TEMPORARY!!!
+              if (_.some(self.playedIDs, function (pID) { return pID == response.song.id;})) {
+                console.log("Song:", response, "already played!");
+                tryGetNewSong(songzaPromise);
+                return false;
+              }
+              self.playedIDs.push(response.song.id)
+              self.playedCount = self.playedIDs.length;
+              self.updateShortDescription();
+
+              // TODO: As songUnresolved:
+              var songSongza = new song(response, songCommons.songType.songza, self);
+              songzaPromise.resolve(songSongza);
+              if (!_.isUndefined(playlistCallback))
+                playlistCallback(songSongza);
+              return true;
+            },
+            /*error callback*/function (error) {
+              console.warn("Error trying get new songza song: " + error);
+              setInterval(tryGetNewSong(songzaPromise), 1000);
+            });
+
+        }
+
+        self.getNext = function (playlistCallback) {
+          var songzaPromise = $q.defer();
+          tryGetNewSong(songzaPromise, playlistCallback);
+          return songzaPromise.promise;
+        }
+        self.updateShortDescription = function () {
+          if (!_.isUndefined(this.station))
+            this.shortDescription = this.station.name;
+          else
+            this.shortDescription = "???";
+        }
+        self.getPlaylistDescription = function () {
+          return this.shortDescription + " (" + this.playedCount + "/" + this.songsCount + ")";
+        }
+      }
+
+      return function songzaStationEntry(station) {
+        commonInit(this);
+
+        if (_.isUndefined(station)) return;
+
+        this.id = station.id;
+        this.station = station;
+        this.stationName = station.name;
+        this.songsCount = station.song_count;
+
+        this.updateShortDescription();
+      };
+    });
+})
+();
