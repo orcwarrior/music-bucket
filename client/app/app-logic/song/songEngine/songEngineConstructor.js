1bbd21035d06982baa33b11048d736af65306747
orcwarrior <orcwarrior@o2.pl>
2015-03-14 14:19:35 +0100

    getDuration method injected into metainfos in songEngineConstructor.

--- client/app/app-logic/song/songEngine/songEngineConstructor.js
+++ client/app/app-logic/song/songEngine/songEngineConstructor.js
@@ -3,17 +3,27 @@
  */
 
 (function () {
-angular.module('musicBucketEngine')
-  .factory('songEngineConstructor', function (songCommons, songEngineYoutube, songEngineSM2) {
+  angular.module('musicBucketEngine')
+    .factory('songEngineConstructor', function (songCommons, songEngineYoutube, songEngineSM2) {
 
-    return function (metainfos, type) {
-      switch (type) {
-        case (songCommons.songType.youtube) :
-          return new songEngineYoutube(metainfos);
-        default :
-          return new songEngineSM2(metainfos);
-      }
-    };
+      return function (metainfos, type) {
+        var engine;
+        switch (type) {
+          case (songCommons.songType.youtube):
+            engine = new songEngineYoutube(metainfos);
+            break;
+          default:
+            engine = new songEngineSM2(metainfos);
+            break;
+        }
+        // inject get duration method to metainfos
+        metainfos.getDuration = function () {
+          var dur = _.bind(engine.getDuration, engine)();
+          if (dur.error || _.isUndefined(dur.result) || dur.result == null) return -1;
+          else return dur.result;
+        };
+        return engine;
+      };
 
-  });
+    });
 })();
