efa294df771ee79bea4587753708c6b4306ccfad
orcwarrior <orcwarrior@o2.pl>
2015-03-16 01:05:19 +0100

    songCommons removed unnecessary stuff.

--- client/app/app-logic/song/songCommons.js
+++ client/app/app-logic/song/songCommons.js
@@ -5,21 +5,8 @@
 (function () {
   angular.module('musicBucketEngine')
     .factory('songCommons', function () {
-               var guid = (function () {
-                 function s4() {
-                   return Math.floor((1 + Math.random()
-                                     ) * 0x10000)
-                     .toString(16)
-                     .substring(1);
-                 }
-               }
-               )();
                return {
-                 songType    : {songza: "typeSongza", local: "typeLocal", spotify: "typeSpotify", youtube: "typeYoutube"},
-                 generateGUID: function () {
-                   return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-                     s4() + '-' + s4() + s4() + s4();
-                 }
+                 songType    : {songza: "typeSongza", local: "typeLocal", spotify: "typeSpotify", youtube: "typeYoutube"}
                }
   });
 })();
