40fc4fefd8d5efb6ca450a1309ba570c04854691
orcwarrior <orcwarrior@o2.pl>
2015-03-26 14:39:41 +0100

    Removed hardcoded ytUrl

--- client/app/routes/youtube/youtube.controller.js
+++ client/app/routes/youtube/youtube.controller.js
@@ -2,7 +2,7 @@
 
 angular.module('musicBucketApp')
   .controller('YoutubeCtrl', function ($scope, youtubeEntry, mbPlayerEngine) {
-    $scope.ytUrl = "https://www.youtube.com/watch?v=hZvFGEE26vE";
+    $scope.ytUrl = "";
 
     $scope.addYTEntry = function () {
       mbPlayerEngine.getPlaylist().addEntry( new youtubeEntry($scope.ytUrl));
