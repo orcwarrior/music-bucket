ae58eab5eb231f0c39c9698c9f27f2032a0bcef5
orcwarrior <orcwarrior@o2.pl>
2015-03-26 14:47:13 +0100

    Queue: Remove song by id.

--- client/app/app-logic/queue/queue.js
+++ client/app/app-logic/queue/queue.js
@@ -7,47 +7,48 @@
 (function () {
   angular.module('musicBucketEngine')
     .factory('queue', function (queueEntry) {
-               return {
-                 constructor: function () {
+      return {
+        constructor: function () {
 
-                   this.entries = [];
-                   this.hasNext = function () {
-                     return this.entries.length > 0;
-                   }
-                   this.peekNextSong = function() {
-                     if (!this.hasNext()) return null;
-                      return _.first(this.entries).song;
-                   }
-                   this.dequeue = this.getNext = function () {
-                     if (this.hasNext()) {
-                        var next = _.first(this.entries);
-                       this.entries = _.rest(this.entries);
-                       return next;
-                     }
-                     return null;
-                   }
-                   this.enqueue = function (song) {
-                     // create queueEntry, add to entries array
-                     this.entries.push(new queueEntry(song));
-                   }
-                   this.enqueueNext = function (song) {
-                     var head = new queueEntry(song);
-                     if (this.hasNext() && this.entries[0].buffered)
-                        head.song.buffer();
-                      this.entries.unshift(head);
-                   }
-                   this.bufferNext = function () {
-                     if (this.hasNext()) {
-                       var next = _.first(this.entries);
-                       if (next.song.buffer()) console.log("Next song in queue buffered!", next.song.shared);
-                     }
-                   }
-                   this.getLenght = function () { return this.entries.length;}
+          this.entries = [];
+          this.hasNext = function () {
+            return this.entries.length > 0;
+          }
+          this.peekNextSong = function () {
+            if (!this.hasNext()) return null;
+            return _.first(this.entries).song;
+          }
+          this.dequeue = this.getNext = function () {
+            if (this.hasNext()) {
+              var next = _.first(this.entries);
+              this.entries = _.rest(this.entries);
+              return next;
+            }
+            return null;
+          }
+          this.enqueue = function (song) {
+            // create queueEntry, add to entries array
+            this.entries.push(new queueEntry(song));
+          }
+          this.enqueueNext = function (song) {
+            var head = new queueEntry(song);
+            if (this.hasNext() && this.entries[0].buffered)
+              head.song.buffer();
+            this.entries.unshift(head);
+          }
+          this.bufferNext = function () {
+            if (this.hasNext()) {
+              var next = _.first(this.entries);
+              if (next.song.buffer()) console.log("Next song in queue buffered!", next.song.shared);
+            }
+          }
+          this.getLenght = function () { return this.entries.length;}
+          this.removeBySongId = function (songId) {
+            this.entries = _.filter(this.entries, function (entry) { return entry.song.metainfos.id !== songId; });
+          };
 
-
-                   return this;
-                 }
-               };
-             });
-}
-)();
+          return this;
+        }
+      };
+    });
+})();
