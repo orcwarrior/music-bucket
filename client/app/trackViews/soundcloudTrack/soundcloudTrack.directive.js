c675bcf586cd46c7a60d1463ff8fdd55fc6450cb
orcwarrior <orcwarrior@o2.pl>
2015-04-20 14:08:24 +0200

    soundcloudTrack

--- client/app/trackViews/soundcloudTrack/soundcloudTrack.directive.js
+++ client/app/trackViews/soundcloudTrack/soundcloudTrack.directive.js
@@ -1,19 +1,22 @@
 'use strict';
 
 angular.module('musicBucketApp')
-  .directive('soundcloudTrack', function (mbPlayerEngine) {
+  .directive('soundcloudTrack', function (mbPlayerEngine, soundcloudEntry) {
     return {
       templateUrl: 'app/trackViews/soundcloudTrack/soundcloudTrack.html',
       restrict: 'EA',
       scope: {
-         track: '=track'
+        track: '=track'
       },
       link: function (scope, element, attrs) {
-        scope.addToPlaylist = function(track) {
-          mbPlayerEngine.addToPlaylist(null);
+        scope.addToPlaylist = function (track) {
+          mbPlayerEngine.addToPlaylist(new soundcloudEntry(track));
         }
         scope.changeArtworkSizeUrl = function (url) {
-          return url.replace("-large.jpg","-crop.jpg");
+          if (url)
+            return url.replace("-large.jpg", "-crop.jpg");
+          else
+            return "";
         }
       }
     };
