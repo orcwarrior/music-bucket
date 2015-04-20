63631e1d7517eeb3474cade61d78eedd22636c21
orcwarrior <orcwarrior@o2.pl>
2015-04-20 13:54:16 +0200

    Metainfos Youtube: Check if just id or whole snippet is provided.

--- client/app/app-logic/song/songMetainfos/songMetainfosYoutube.js
+++ client/app/app-logic/song/songMetainfos/songMetainfosYoutube.js
@@ -5,13 +5,17 @@
   angular.module('musicBucketEngine')
     .factory('songMetainfosYoutube', function (songMetainfos) {
 
-      var songMetainfosYoutube = function songMetainfosYoutube(id) {
-        this.id = id;
-        this.url = "http://youtube.com/?watch=" + id;
+      var songMetainfosYoutube = function songMetainfosYoutube(providedInfos) {
+        if (_.isObject(providedInfos)) {
+          this.id = providedInfos.id.videoId;
+          this.title = providedInfos.snippet.title;
+        } else {
+          this.id = providedInfos;
+        }
+        this.url = "http://youtube.com/?watch=" + this.id;
         this.getUrl = function () { return this.url; };
       };
       songMetainfosYoutube.prototype = new songMetainfos();
       return songMetainfosYoutube;
-
     });
 })();
