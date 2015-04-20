90e8cc80a9e2171cddd76e3d787f833a0e256cd9
orcwarrior <orcwarrior@o2.pl>
2015-04-20 13:57:26 +0200

    Soundcloud search.

--- client/app/routes/soundcloud/soundcloud.controller.js
+++ client/app/routes/soundcloud/soundcloud.controller.js
@@ -1,13 +1,17 @@
 'use strict';
 
 angular.module('musicBucketApp')
-  .controller('SoundcloudCtrl', function ($scope, soundcloudApi) {
+  .controller('SoundcloudCtrl', function ($scope, $location, soundcloudApi) {
     $scope.message = 'Hello';
+    $scope.searchQuery = $location.search().query;
 
     $scope.search = function(query) {
+      $location.search('query', query);
+      $location.replace();
       soundcloudApi.search.track(query)
         .then( function (response) {
           $scope.searchResults = response.data;
         });
-    }
+    };
+    if ($scope.searchQuery) $scope.search($scope.searchQuery);
   });
