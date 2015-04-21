b425aedcde58d395a6495a0f5fb18a0c8bb76fe9
orcwarrior <orcwarrior@o2.pl>
2015-04-21 10:33:43 +0200

    songSeeker

---
+++ client/app/app-logic/song/songSeeker.js
@@ -0,0 +1,49 @@
+/**
+ * Created by orcwarrior on 2015-04-12.
+ */
+
+angular.module('musicBucketEngine')
+  .factory('songSeeker', function ($injector, $q, songCommons, youtubeApi, soundcloudApi) {
+    function buildSearchQuery(metainfos) {
+      if (!_.isUndefined(metainfos.artist)) {
+        return metainfos.artist + " " + metainfos.title;
+      } else {
+        return metainfos.title;
+      }
+    }
+
+
+    var SEEKING_SERVICES = 2;
+    return function songSeeker(metainfos) {
+      var resolvedServices = 0;
+      var searchQuery = buildSearchQuery(metainfos);
+      var foundedSongs = [];
+      var song = $injector.get('song');
+      var deferred = $q.defer();
+
+      youtubeApi.search(searchQuery)
+        .then(function (response) {
+          var firstEntry = _.first(response.data.items);
+          if (firstEntry) {
+            var ytSong = new song(firstEntry, songCommons.songType.youtube);
+            ytSong.metainfos = _.extend(ytSong.metainfos, metainfos);
+            foundedSongs.push(ytSong);
+          }
+          if (++resolvedServices >= SEEKING_SERVICES)
+            deferred.resolve(foundedSongs);
+        });
+      soundcloudApi.search.track(searchQuery, 5)
+        .then(function (response) {
+          if (response.data.length > 0) {
+            var scSong = new song(response.data[0], songCommons.songType.soundcloud);
+            scSong.metainfos = _.extend(scSong.metainfos, metainfos);
+            foundedSongs.push(scSong);
+          }
+          if (++resolvedServices >= SEEKING_SERVICES)
+            deferred.resolve(foundedSongs);
+        });
+
+      return deferred.promise;
+    };
+  }
+);
