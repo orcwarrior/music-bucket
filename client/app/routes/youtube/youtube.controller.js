5b96247d421ec8d8cb99993c948f90646bad9739
orcwarrior <orcwarrior@o2.pl>
2015-07-23 19:16:10 +0200

    Youtube view - now search implemented, view based on md-grid, still ability to paste url's to playlists/videos.

--- client/app/routes/youtube/youtube.controller.js
+++ client/app/routes/youtube/youtube.controller.js
@@ -1,11 +1,33 @@
 'use strict';
 
 angular.module('musicBucketApp')
-  .controller('YoutubeCtrl', function ($scope, youtubeEntry, mbPlayerEngine) {
-    $scope.ytUrl = "";
+  .controller('YoutubeCtrl', function ($scope, $location, youtubeEntry, mbPlayerEngine, youtubeApi) {
 
-    $scope.addYTEntry = function () {
-      mbPlayerEngine.getPlaylist().addEntry( new youtubeEntry($scope.ytUrl));
+    $scope.addYTEntry = function (url) {
+      mbPlayerEngine.getPlaylist().addEntry(new youtubeEntry(url));
+    };
+    $scope.ytSearch = function (searchQuery) {
+      youtubeApi.search($scope.searchQuery, 25, {})
+        .then(function (response) {
+          $scope.ytClips = response.data.items;
+        });
+      youtubeApi.search($scope.searchQuery, 25, {type: 'playlist'})
+        .then(function (response) {
+          $scope.ytPlaylists = response.data.items;
+        });
+
+    };
+    $scope.inteliSearch = function (str) {
+      $location.search('query', str);
+      $location.replace();
+
+      if (str.indexOf("v=") !== -1 || str.indexOf("list=") !== -1)
+        $scope.addYTEntry(str);
+      else
+        $scope.ytSearch(str);
     };
 
+    $scope.ytUrl = "";
+    $scope.searchQuery = $location.search().query;
+    if ($scope.searchQuery) $scope.inteliSearch($scope.searchQuery);
   });
