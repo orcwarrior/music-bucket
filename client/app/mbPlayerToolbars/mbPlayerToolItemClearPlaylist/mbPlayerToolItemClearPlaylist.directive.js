d451eef97c3a6fed5ac57259b15bc63d5d910d37
orcwarrior <orcwarrior@o2.pl>
2015-07-23 18:59:02 +0200

    ToolItem: Clear playlist

---
+++ client/app/mbPlayerToolbars/mbPlayerToolItemClearPlaylist/mbPlayerToolItemClearPlaylist.directive.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('musicBucketApp')
+  .directive('mbPlayerToolItemClearPlaylist', function (mbPlayerEngine) {
+    return {
+      templateUrl: 'app/mbPlayerToolbars/mbPlayerToolItemClearPlaylist/mbPlayerToolItemClearPlaylist.html',
+      restrict: 'EA',
+      link: function (scope, element, attrs) {
+        scope.getPlaylist = function () {
+          return mbPlayerEngine.getPlaylist();
+        };
+      }
+    };
+  });
