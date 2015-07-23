f7758bbdea81d9501509f134d99de2f2b5146bee
orcwarrior <orcwarrior@o2.pl>
2015-07-23 18:50:59 +0200

    songza-api-proxy - tuned 4 new php proxy

--- server/proxy/songza-api-proxy/index.js
+++ server/proxy/songza-api-proxy/index.js
@@ -6,6 +6,7 @@
 var express = require('express');
 var config = require('../../config/environment');
 var http = require('http');
+var _ = require('lodash');
 
 var httpProxy = require('http-proxy');
 var proxy = httpProxy.createProxyServer({xfwd : true, changeOrigin: true, hostRewrite: true });
@@ -14,17 +15,22 @@ var proxy = httpProxy.createProxyServer({xfwd : true, changeOrigin: true, hostRe
 module.exports = function(req, res) {
   var pattern = /\/songza-api-proxy\/(.*)/;
   if (pattern.exec(req.url)) {
-    var proxyHost = 'http://transparent-proxy.herokuapp.com/proxy.php?' /*+ req.url.match(pattern)[2] + '&*/ + '__dest_url=/' + req.url.match(pattern)[1];
-    console.log(proxyHost);
-    res.setHeader('Access-Control-Allow-Origin', 'http://songza.com');
-    res.setHeader('Access-Control-Request-Headers', 'accept, authorization');
-    req.setHeader('Access-Control-Request-Headers', 'accept, authorization');
+    var proxyHost = 'http://transparent-proxy.herokuapp.com/proxy.php?' /*+ req.url.match(pattern)[2] + '&*/ + '__dest_url=/' + req.url.match(pattern)[1].replace('?', '&') + "&client=mb";
+    console.warn("->" + proxyHost);
 
-    // req.headers['Access-Control-Allow-Origin'] = req.headers['origin'];
-    // req.headers['Access-Control-Allow-Credentials'] = "true";
-    res.setHeader('Access-Control-Allow-Origin', config.ip);
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
+      // remove cookie header:
+    console.warn(req.cookies);
+      req.cookies = {};
+    console.warn(req.cookies);
+      res.setHeader('Access-Control-Allow-Origin', 'http://songza.com');
+      res.setHeader('Access-Control-Request-Headers', 'accept, authorization');
+      if (_.isFunction(req.setHeader))
+      req.setHeader('Access-Control-Request-Headers', 'accept, authorization');
 
+      // req.headers['Access-Control-Allow-Origin'] = req.headers['origin'];
+      // req.headers['Access-Control-Allow-Credentials'] = "true";
+      res.setHeader('Access-Control-Allow-Origin', config.ip);
+      res.setHeader('Access-Control-Allow-Credentials', 'true');
     try {
       proxy.web(req, res, { target: proxyHost });
     }
