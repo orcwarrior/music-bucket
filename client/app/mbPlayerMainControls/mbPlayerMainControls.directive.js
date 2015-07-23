5642134b8a67d9b10d0ac64c7ee0569bb29540c3
orcwarrior <orcwarrior@o2.pl>
2015-07-23 18:56:59 +0200

    SM2/YT helper functions

--- client/app/mbPlayerMainControls/mbPlayerMainControls.directive.js
+++ client/app/mbPlayerMainControls/mbPlayerMainControls.directive.js
@@ -5,7 +5,7 @@ angular.module('musicBucketApp')
     return {
       templateUrl: 'app/mbPlayerMainControls/mbPlayerMainControls.html',
       restrict: 'E',
-      scope : {},
+      scope: {},
       link: function (scope, element, attrs) {
         scope.player = mbPlayerEngine;
 
@@ -13,11 +13,27 @@ angular.module('musicBucketApp')
           if (_.isNull(song)) return '';
           return song.metainfos.getSongDescription();
         };
-        scope.getLastSongDescription = function() {
+        scope.getLastSongDescription = function () {
           if (_.isUndefined(mbPlayerEngine.tracksHistory) || _.isUndefined(mbPlayerEngine.tracksHistory.peekLastSong()))
             return "";
           else
             return scope.getSongDescription(mbPlayerEngine.tracksHistory.peekLastSong());
+        };
+
+
+        scope.isSM2Song = function () {
+          var song = mbPlayerEngine.getCurrentSong();
+          if (_.isUndefined(song)) return false;
+          var engine = song.getUsedEngine();
+          if (_.isUndefined(engine)) return false;
+          return engine.name == "songEngineSM2";
+        }
+        scope.isYoutubeSong = function () {
+          var song = mbPlayerEngine.getCurrentSong();
+          if (_.isUndefined(song)) return false;
+          var engine = song.getUsedEngine();
+          if (_.isUndefined(engine)) return false;
+          return engine.name == "songEngineYoutube";
         }
       }
     };
