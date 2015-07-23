b822fe64c78b893823929cf40615773b24bfbf76
orcwarrior <orcwarrior@o2.pl>
2015-07-23 18:59:40 +0200

    ToolItem - save playlist now open save form in modal.

--- client/app/mbPlayerToolbars/mbPlayerToolItemSavePlaylist/mbPlayerToolItemSavePlaylist.directive.js
+++ client/app/mbPlayerToolbars/mbPlayerToolItemSavePlaylist/mbPlayerToolItemSavePlaylist.directive.js
@@ -1,17 +1,35 @@
 'use strict';
 
 angular.module('musicBucketApp')
-  .directive('mbPlayerToolItemSavePlaylist', function (mbPlayerEngine, playlistService) {
+  .directive('mbPlayerToolItemSavePlaylist', function ($mdDialog, mbPlayerEngine, playlistService) {
     return {
       templateUrl: 'app/mbPlayerToolbars/mbPlayerToolItemSavePlaylist/mbPlayerToolItemSavePlaylist.html',
       restrict: 'EA',
-        link: function (scope, element, attrs) {
-          scope.player = mbPlayerEngine;
+      link: function (scope, element, attrs) {
+        scope.player = mbPlayerEngine;
+
         scope.savePlaylist = function () {
           if (!_.isUndefined(mbPlayerEngine.getPlaylist())) {
             playlistService.save(mbPlayerEngine.getPlaylist());
           }
         };
+        scope.savePlaylistDialog = function (ev) {
+          $mdDialog.show({
+            controller: 'PlaylistSaveDialogController',
+            templateUrl: 'app/templates/playlist.edit.template.html',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            locals: {
+              'playlist': mbPlayerEngine.playlist
+            },
+          })
+            .then(function (answer) {
+              scope.alert = 'You said the information was "' + answer + '".';
+            }, function () {
+              scope.alert = 'You cancelled the dialog.';
+            });
+
+        }
       }
     };
   });
