7a67cfd7e4db52ea442a5bfaa38a1d5ec05bd9ac
orcwarrior <orcwarrior@o2.pl>
2015-07-23 19:07:17 +0200

    Playlist view GET params/ style fixes

--- client/app/routes/playlists/playlists.js
+++ client/app/routes/playlists/playlists.js
@@ -4,7 +4,7 @@ angular.module('musicBucketApp')
   .config(function ($stateProvider) {
       $stateProvider
         .state('main.playlists',{
-                 url : '/playlists',
+                 url : '/playlists?autoplay&id&theater',
                  templateUrl: 'app/routes/playlists/playlists.html',
                  controller : 'PlaylistsCtrl'
                });
