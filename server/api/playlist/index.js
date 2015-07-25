9314e7b6ad178be83873fca7538c338ef32b480a
orcwarrior <orcwarrior@o2.pl>
2015-07-25 14:31:17 +0200

    Backend: Playlist advance timer logic.

--- server/api/playlist/index.js
+++ server/api/playlist/index.js
@@ -6,11 +6,12 @@ var auth = require('../../auth/auth.service');
 
 var router = express.Router();
 
-router.get('/', controller.index);
+router.get('/', auth.extractUser() , controller.index);
 router.get('/:id', controller.show);
 router.post('/', controller.create);
 router.put('/:id', auth.isAuthenticated(), controller.update);
 router.patch('/:id', auth.isAuthenticated(), controller.update);
 router.delete('/:id', auth.isAuthenticated(), controller.destroy);
+router.post('/:id/advanceTimer', controller.advanceTimer);
 
 module.exports = router;
--- server/api/playlist/playlist.controller.js
+++ server/api/playlist/playlist.controller.js
@@ -4,40 +4,54 @@ var _ = require('lodash');
 var Playlist = require('./playlist.model');
 
 // Get list of playlists
-exports.index = function(req, res) {
+exports.index = function (req, res) {
   var uId = (_.isUndefined(req.user)) ? null : req.user._id;
-  Playlist.find({ $or:
-                  [ {visibility: { $in : ["public", null] }},
-                    {author: uId, visibility: "private"}]
-                }, function (err, playlists) {
-    if(err) { return handleError(res, err); }
+  Playlist.find({
+    $or: [{visibility: {$in: ["public", null]}},
+      {author: uId, visibility: "private"}]
+  }, function (err, playlists) {
+    if (err) {
+      return handleError(res, err);
+    }
     return res.json(200, playlists);
   });
 };
 
 // Get a single playlist
-exports.show = function(req, res) {
+exports.show = function (req, res) {
   Playlist.findById(req.params.id, function (err, playlist) {
-    if(err) { return handleError(res, err); }
-    if(!playlist) { return res.send(404); }
+    if (err) {
+      return handleError(res, err);
+    }
+    if (!playlist) {
+      return res.send(404);
+    }
     return res.json(playlist);
   });
 };
 
 // Creates a new playlist in the DB.
-exports.create = function(req, res) {
-  Playlist.create(req.body, function(err, playlist) {
-    if(err) { return handleError(res, err); }
+exports.create = function (req, res) {
+  Playlist.create(req.body, function (err, playlist) {
+    if (err) {
+      return handleError(res, err);
+    }
     return res.json(201, playlist);
   });
 };
 
 // Updates an existing playlist in the DB.
-exports.update = function(req, res) {
-  if(req.body._id) { delete req.body._id; }
+exports.update = function (req, res) {
+  if (req.body._id) {
+    delete req.body._id;
+  }
   Playlist.findById(req.params.id, function (err, playlist) {
-    if (err) { return handleError(res, err); }
-    if(!playlist) { return res.send(404); }
+    if (err) {
+      return handleError(res, err);
+    }
+    if (!playlist) {
+      return res.send(404);
+    }
 
     // DK: Check if current user is author of an playlist:
     if (playlist.author.toString() !== req.user.id) {
@@ -51,9 +65,13 @@ exports.update = function(req, res) {
     console.log(updated);
     // DK: Otherwise mongo don't see difference in documents
     playlist.remove(function (err) {
-      if (err) { return handleError(res, err); }
+      if (err) {
+        return handleError(res, err);
+      }
       Playlist.create(updated, function (err, playlist) {
-        if (err) { return handleError(res, err); }
+        if (err) {
+          return handleError(res, err);
+        }
         return res.json(200, playlist);
       });
     });
@@ -61,21 +79,45 @@ exports.update = function(req, res) {
 };
 
 // Deletes a playlist from the DB.
-exports.destroy = function(req, res) {
+exports.destroy = function (req, res) {
   Playlist.findById(req.params.id, function (err, playlist) {
-    if(err) { return handleError(res, err); }
-    if(!playlist) { return res.send(404); }
+    if (err) {
+      return handleError(res, err);
+    }
+    if (!playlist) {
+      return res.send(404);
+    }
 
     // DK: Check if current user is author of an playlist:
     if (playlist.author.toString() !== req.user.id) {
       return res.send(401);
     }
-    playlist.remove(function(err) {
-      if(err) { return handleError(res, err); }
+    playlist.remove(function (err) {
+      if (err) {
+        return handleError(res, err);
+      }
       return res.send(204);
     });
   });
 };
+
+exports.advanceTimer = function (req, res) {
+  Playlist.findById(req.params.id, function (err, playlist) {
+    if (err) {
+      return handleError(res, err);
+    }
+    if (!playlist) {
+      return res.send(404);
+    }
+
+    playlist.totalPlaytime++;
+    playlist.save(function(err) {
+      if (err) return handleError(res, err);
+      res.send(200);
+    });
+
+  });
+}
 
 function handleError(res, err) {
   return res.send(500, err);
