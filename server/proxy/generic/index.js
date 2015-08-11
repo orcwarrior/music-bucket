cf649694655fd3a5809085068e1a28208e5b35a2
orcwarrior <orcwarrior@o2.pl>
2015-08-12 01:37:45 +0200

    Generic proxy, so you can threat resources as local :)

---
+++ server/proxy/generic/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var express = require('express');
+var config = require('../../config/environment');
+
+var request = require('request');
+// Middleware:
+module.exports = function (req, res) {
+  var pattern = /\/proxy\/(.*)/;
+  //var proxyHost = 'http://coverartarchive.org/release/' + req.url.match(pattern)[1];
+  //var proxyPath = '/release/' + req.url.match(pattern)[1];
+  var proxyPath = req.url.match(pattern)[1] + "";
+
+  console.warn("generic proxy requested, proxyPath: " + proxyPath + ", match: " + req.url.match(pattern)[1]);
+  if (!proxyPath || !proxyPath.length) {
+    res.status(500);
+    res.end();
+    return;
+  };
+
+  request(proxyPath, {timeout : 60000})
+    .pipe(res);
+};
--- server/routes.js
+++ server/routes.js
@@ -21,6 +21,7 @@ module.exports = function(app) {
   app.all('/songza-api-proxy/*', require('./proxy/songza-api-proxy'));
   app.all('/musicbrainz-api/*', require('./proxy/musicbrainz-api'));
   app.all('/coverartarchive-api/*', require('./proxy/coverartarchive-api'));
+  app.all('/proxy/*', require('./proxy/generic'));
 
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
