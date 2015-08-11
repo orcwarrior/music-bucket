9a9fa1efaa1726b6d2c80a872ed46f5ea31aaffd
orcwarrior <orcwarrior@o2.pl>
2015-08-12 01:36:25 +0200

    Primitive extracting infos from yt clip name.

--- client/app/app-logic/song/songMetainfos/songMetainfosYoutube.js
+++ client/app/app-logic/song/songMetainfos/songMetainfosYoutube.js
@@ -7,14 +7,22 @@
 
       var songMetainfosYoutube = function songMetainfosYoutube(providedInfos) {
         if (_.isObject(providedInfos)) {
+          var combTitle = providedInfos.snippet.title;
           this.id = providedInfos.id.videoId;
-          this.title = providedInfos.snippet.title;
+          this.artist = combTitle.split('-')[0];
+          this.title = combTitle.split('-')[1];
         } else {
           this.id = providedInfos;
         }
         this.albumArt = "http://img.youtube.com/vi/"+this.id+"/hqdefault.jpg";
         this.url = "http://youtube.com/?watch=" + this.id;
         this.getUrl = function () { return this.url; };
+      };
+      songMetainfosYoutube.extractArtistAndTitle = function(videoTitle) {
+        var res = {artist:'', title:''};
+        res.artist = videoTitle.split(' -')[0].trim();
+        res.title = videoTitle.split(' -')[1].trim();
+        return res;
       };
       songMetainfosYoutube.prototype = new songMetainfos();
       return songMetainfosYoutube;
