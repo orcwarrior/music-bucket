c891648b565652261b10ce0caf8278c54f6edca8
orcwarrior <orcwarrior@o2.pl>
2015-08-12 01:36:00 +0200

    Youtube entry view: added functionality to show more infos about clip (plus advanced actions)

---
+++ client/app/templates/ytEntry.template.controller.js
@@ -0,0 +1,38 @@
+/**
+ * Created by orcwarrior on 2015-07-23.
+ */
+
+angular.module('musicBucketApp')
+  .controller('YTEntryDialogController', function ($mdDialog, $scope, entry, mbPlayerEngine) {
+
+    $scope.entry = entry;
+    $scope.doAction = function (action) {
+      var ytEntry = new youtubeEntry($scope.entry.id);
+      mbPlayerEngine.addToPlaylist(ytEntry);
+      switch (action) {
+        case "add":
+          break;
+        case "play":
+          mbPlayerEngine.entryPlay(ytEntry);
+          break;
+        case "play-next":
+          mbPlayerEngine.entryPlayNext(ytEntry);
+          break;
+        case "queue":
+          mbPlayerEngine.entryEnqueue(ytEntry);
+          break;
+      };
+      $scope.cancel();
+    };
+    $scope.hide = function () {
+      $mdDialog.hide();
+    };
+    $scope.cancel = function () {
+      $mdDialog.cancel();
+    };
+    $scope.answer = function (answer) {
+      // if (answer === 'save')
+      //    playlistService.save($scope.playlist);
+      $mdDialog.hide(answer);
+    };
+  });
--- client/app/trackViews/youtubeEntry/youtubeEntry.directive.js
+++ client/app/trackViews/youtubeEntry/youtubeEntry.directive.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('musicBucketApp')
-  .directive('youtubeEntry', function (mbPlayerEngine, youtubeEntry) {
+  .directive('youtubeEntry', function (mbPlayerEngine, youtubeEntry, $mdDialog) {
     return {
       templateUrl: 'app/trackViews/youtubeEntry/youtubeEntry.html',
       restrict: 'EA',
@@ -12,6 +12,17 @@ angular.module('musicBucketApp')
         scope.addToPlaylist = function (entry) {
           var ytEntry = new youtubeEntry(entry.id);
           mbPlayerEngine.addToPlaylist(ytEntry);
+        };
+        scope.moreInfos = function (ev) {
+          $mdDialog.show({
+            controller: 'YTEntryDialogController',
+            templateUrl: 'app/templates/ytEntry.view.template.html',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            locals: {
+              'entry': scope.entry
+            }
+          });
         };
       }
     };
