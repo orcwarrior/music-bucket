7472796ebbbb84b94f8a73b8eda536d11a66ce46
orcwarrior <orcwarrior@o2.pl>
2015-09-19 18:40:47 +0200

    backend: proxy/generic error handling

--- server/proxy/generic/index.js
+++ server/proxy/generic/index.js
@@ -12,12 +12,18 @@ module.exports = function (req, res) {
   var proxyPath = req.url.match(pattern)[1] + "";
 
   console.warn("generic proxy requested, proxyPath: " + proxyPath + ", match: " + req.url.match(pattern)[1]);
-  if (!proxyPath || !proxyPath.length) {
+  if (!proxyPath || proxyPath === "") {
     res.status(500);
     res.end();
     return;
-  };
+  }
 
-  request(proxyPath, {timeout : 60000})
-    .pipe(res);
+  request(proxyPath, {timeout: 15000})
+    .on('error', function (err) {
+      console.log(err);
+    })
+    .pipe(res).on('error', function (e) {
+      res.status(500);
+      res.json(e);
+    });
 };
