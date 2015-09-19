d2b7162629a9eae8238627e1db15f365f5c45a1f
orcwarrior <orcwarrior@o2.pl>
2015-09-19 04:03:07 +0200

    Discover view - lots of refactoring such wow

--- client/app/routes/discover/discover.controller.js
+++ client/app/routes/discover/discover.controller.js
@@ -1,43 +1,155 @@
 'use strict';
 
 angular.module('musicBucketApp')
-  .controller('discoverCtrl', function ($window, $scope, $timeout, $location, mbPlayerEngine, lastFmApi, songBuilder) {
+  .factory('discoverMetainfos', function ($log, $location, songBuilder, lastFmApi, mbPlayerEngine) {
+    var prevDiscoverMetainfos = undefined;
+    var discoverMetainfosHistory = [];
 
-    $scope.artist = {};
-    var lastSong = {};
-    var storedPlaysCounts = [];
-    var bioAlreadyLoaded = false;
-
-    if (!_.isEmpty(_buildParamsSongPlaceholder())) {
-      var paramsSong = _buildParamsSongPlaceholder();
-      initInfos(paramsSong);
+    function discoverMetainfosEntry(config, discoverInfos, palette) {
+      this.config = config;
+      this.discoverInfos = discoverInfos;
+      this.palette = palette;
+      return this;
     }
 
-    $scope.curSong = function () {
-      return mbPlayerEngine.getCurrentSong();
+    function discoverMetainfos(createConfig) {
+
+      var discoverMetainfosInstance = this;
+      if (createConfig && createConfig.metainfos) {
+        createConfig = {
+          artist: createConfig.metainfos.artist,
+          album: createConfig.metainfos.album,
+          title: createConfig.metainfos.title
+        };
+      }
+      if (!_.isUndefined(prevDiscoverMetainfos) && __discoverConfigEquality(createConfig, prevDiscoverMetainfos.infos))
+        return prevDiscoverMetainfos; // return old, there is no reason to fire same requests...
+
+      // Pass old palette obj to new discoverMetainfos for meaningful transition:
+      if (!_.isUndefined(prevDiscoverMetainfos))
+        this.palette = prevDiscoverMetainfos.palette;
+      // Clear history:
+      discoverMetainfosHistory.length = 0;
+
+      /* private functions */
+      function __discoverConfigEquality(one, two) {
+        one = _.pick(one, 'artist', 'album', 'title');
+        two = _.pick(two, 'artist', 'album', 'title');
+        return _.isEqual(one, two);
+      }
+
+      function __isNewArtist(config) {
+        return config.artist !== "" && _.isUndefined(previousDiscMeta.artist) || previousDiscMeta.artist.toUpperCase() !== config.artist.toUpperCase();
+      }
+
+      function setTrackProgress(idx, percentProgress) {
+        if ($scope.artistTopTracks.track[idx].circleValue === storedPlaysCounts[idx])
+          return; // already loaded.
+        $scope.artistTopTracks.track[idx].circleValue = $scope.artistTopTracks.maxplayscount * percentProgress;
+      }
+
+
+      /* public object*/
+      this.infos = createConfig;
+      this.discoverInfos = {artist: null, artistTopTracks: null, artistTopAlbums: null};
+      this.tmpMetainfos = {};
+      /* observe object */
+      this._observeCb = function (changes) {
+        if (changes[0].object.artist && changes[0].object.artistTopAlbums && changes[0].object.artistTopTracks) {
+          discoverMetainfosInstance.tmpMetainfos.allPrepared = true;
+          discoverMetainfosInstance.discoverInfos = discoverMetainfosInstance.tmpMetainfos;
+          discoverMetainfosInstance.updateUrlParams();
+        }
+      };
+      this.requestInfos = function (partialChangeConfig) {
+        var self = this;
+        var conf = _.clone(this.infos);
+        if (partialChangeConfig && !_.isEqual(partialChangeConfig, this.infos))
+          this.infos = conf = _.extend(conf, partialChangeConfig);
+
+        if (__discoverConfigEquality(conf, this.getPrevious().config) || _.isEmpty(conf))
+          return; // Don't lookup for same config
+        $log.info('New discover request: ');
+        $log.info(conf);
+
+        this.tmpMetainfos = {};
+
+        discoverMetainfosHistory.push(new discoverMetainfosEntry(conf, this.tmpMetainfos));
+        Object.observe(this.tmpMetainfos, this._observeCb);
+        // Artist infos
+        (function artistInfos() {
+          lastFmApi.artist.getInfo(conf.artist)
+            .then(function (response) {
+              if (response.status == 200)
+                self.tmpMetainfos.artist = response.data.artist;
+            });
+          lastFmApi.artist.getTopTracks(conf.artist)
+            .then(function (response) {
+              if (response.status == 200 && response.data.toptracks) {
+                self.tmpMetainfos.artistTopTracks = response.data.toptracks;
+                self.tmpMetainfos.artistTopTracks.maxplayscount = response.data.toptracks.track[0].playcount;
+              }
+            });
+          lastFmApi.artist.getTopAlbums(conf.artist)
+            .then(function (response) {
+              if (response.status == 200 && response.data.topalbums)
+                self.tmpMetainfos.artistTopAlbums = response.data.topalbums.album;
+            });
+        })();
+        return this; // chaining
+      };
+
+      this.buildUrlParamsSongPlaceholder = function () {
+        if ($location.search().artist)
+          this.infos = createConfig = _.pick($location.search(), 'artist', 'album', 'title');
+        return this; // chaining
+      };
+      this.updateUrlParams = function () {
+        console.log('[DiscoverMeta] Update url params');
+        if (_.isUndefined(this.infos)) return this;
+
+        var anyChanges = false;
+        if ($location.search().artist != this.infos.artist
+          || $location.search().title != this.infos.title
+          || $location.search().album != this.infos.album)
+          anyChanges = true;
+
+        $location.search('artist', this.infos.artist);
+        $location.search('title', this.infos.title);
+        $location.search('album', this.infos.album);
+
+        if (anyChanges)
+          $location.replace();
+
+        return this; // chaining
+      };
+
+      prevDiscoverMetainfos = this;
+      return this;
     };
-    $scope.songAction = function (action, query, idx) {
-      // All visual stuff:
-      $scope.artistTopTracks.track[idx].isLoading = true;
-      storedPlaysCounts[idx] = $scope.artistTopTracks.track[idx].playcount;
-      $scope.artistTopTracks.track[idx].circleValue = 0;
-      $timeout(function () {
-        setTrackProgress(idx, 0.5);
-      }, 100);
-      $timeout(function () {
-        setTrackProgress(idx, 1);
-      }, 500);
-      $timeout(function () {
-        setTrackProgress(idx, 0);
-      }, 1000);
-      $timeout(function () {
-        setTrackProgress(idx, 1);
-      }, 1600);
+    discoverMetainfos.prototype.songAction = function (action, query, idx, discoverProperty) {
+      // if (this.discoverInfos.artistTopTracks)
+      // // All visual stuff:
+      // $scope.artistTopTracks.track[idx].isLoading = true;
+      // storedPlaysCounts[idx] = $scope.artistTopTracks.track[idx].playcount;
+      // $scope.artistTopTracks.track[idx].circleValue = 0;
+      // $timeout(function () {
+      //   setTrackProgress(idx, 0.5);
+      // }, 100);
+      // $timeout(function () {
+      //   setTrackProgress(idx, 1);
+      // }, 500);
+      // $timeout(function () {
+      //   setTrackProgress(idx, 0);
+      // }, 1000);
+      // $timeout(function () {
+      //   setTrackProgress(idx, 1);
+      // }, 1600);
 
       songBuilder.createSong(query)
         .then(function (song) {
-          $scope.artistTopTracks.track[idx].circleValue = storedPlaysCounts[idx];
-          $scope.artistTopTracks.track[idx].isLoading = false;
+          // $scope.artistTopTracks.track[idx].circleValue = storedPlaysCounts[idx];
+          // $scope.artistTopTracks.track[idx].isLoading = false;
 
           switch (action) {
             case "play" :
@@ -53,127 +165,79 @@ angular.module('musicBucketApp')
         });
     };
 
+    discoverMetainfos.prototype.markPalette = function (palette) {
+      this.getPrevious().palette = this.palette = palette;
+    }
+    discoverMetainfos.prototype.getPrevious = function () {
+      return _.last(discoverMetainfosHistory) || {};
+    };
+    // For button to get back to playing song metainfos:
+    discoverMetainfos.prototype.hasRootMetainfos = function () {
+      return (discoverMetainfosHistory.length > 1
+      && discoverMetainfosHistory[1].discoverInfos.allPrepared);
+    };
+    discoverMetainfos.prototype.backToRootMetainfos = function () {
+      var root = _.first(discoverMetainfosHistory);
+      this.infos = root.config;
+      this.discoverInfos = root.discoverInfos;
+      this.palette = root.palette;
+      this.updateUrlParams();
+      discoverMetainfosHistory.length = 1;
+    };
+    return discoverMetainfos;
+  }
+)
+  .controller('discoverCtrl', function ($window, $scope, $timeout, $location, mbPlayerEngine, lastFmApi, songBuilder, discoverMetainfos) {
+
+    var lastSong = {};
+    var storedPlaysCounts = [];
+    var bioAlreadyLoaded = false;
+
+    // Params metainfos:
+    $scope.song = new discoverMetainfos().buildUrlParamsSongPlaceholder();
+    $scope.song.requestInfos();
+
     mbPlayerEngine.listen("songChange", function (engine, eventName, song) {
-      initInfos(song);
+      $scope.song = new discoverMetainfos(song);
+      $scope.song.requestInfos();
     });
 
-    function initInfos(song) {
-
-      console.log('[Discover] request artist infos: ' + song.metainfos.artist);
-      /*
-       * Artist column
-       * */
-      if (song.metainfos.artist !== "" && _.isUndefined(lastSong.artist) || lastSong.artist.toUpperCase() !== song.metainfos.artist.toUpperCase()) {
-
-        (function artistInfos() {
-          bioAlreadyLoaded = false;
-          lastFmApi.artist.getInfo(song.metainfos.artist)
-            .then(function (response) {
-              //$scope.$apply(function() {
-              console.log('[Discover] artist data resolved.');
-              $scope.__artist = response.data.artist;
-              //);
-            });
-          lastFmApi.artist.getTopTracks(song.metainfos.artist)
-            .then(function (response) {
-              $scope.artistTopTracks = [];
-              if (bioAlreadyLoaded) {
-                $scope.artistTopTracks = response.data.toptracks;
-                $scope.artistTopTracks.maxplayscount = $scope.artistTopTracks.track[0].playcount;
-                $timeout(function () {
-                  $scope.$broadcast('discover:artist:updated');
-                }, 500);
-                $timeout(function () {
-                  $scope.$broadcast('discover:artist:updated');
-                }, 1500);
-              } else {
-                $scope.__artistTopTracks = response.data.toptracks;
-              }
-            });
-        })();
-      }
-      else
-        console.info("[Discover] arist was already loaded: " + lastSong.artist);
-
-      /*
-       * Album column
-       * */
-      // TO DO Some parraler task management etc.
-      lastSong.artist = song.metainfos.artist;
-      lastSong.title = song.metainfos.title;
-      lastSong.album = song.metainfos.album;
-    }
-
-    if (!_.isUndefined($scope.curSong()))
-      initInfos($scope.curSong());
-
-    function paletteChangeHandler() {
-      var img = document.querySelector('.artist-col-wrapper #artistImgPalette');
-      angular.element(img).bind('paletteReady', function (srd, eventName, palette) {
-        console.log('[Discover] artist image loaded. (showing new infos)');
-        $scope.artist = $scope.__artist;
-        $scope.artist.imageUrl = $scope.__artist.image[4]['#text']
-          || 'http://i.imgur.com/ha6KE5e.jpg';
-        $scope.$broadcast('discover:artist:updated');
-
-
-        bioAlreadyLoaded = true;
-        $scope.artistTopTracks = $scope.__artistTopTracks;
-        $scope.__artistTopTracks.maxplayscount = $scope.__artistTopTracks.track[0].playcount;
-        $timeout(function () {
-          $scope.$broadcast('discover:artist:updated');
-        }, 500);
-        // Update url params when transistions are done :)
-        $timeout(function () {
-          _updateUrlParams(lastSong);
-        }, 1000)
-      });
+    if (!_.isUndefined($scope.curSong())) {
+      $scope.song = new discoverMetainfos($scope.curSong());
+      $scope.song.requestInfos();
     };
-    $timeout(function() {
-      paletteChangeHandler();
+
+    $timeout(function paletteChangeHandler() {
+      var img = document.querySelector('.artist-col-wrapper #artistImgPalette');
+      angular.element(img).bind('paletteReady', function (event, palette) {
+        console.log('[Discover] artist image loaded. (showing new infos)');
+        $scope.$broadcast('discover:artist:updated');
+        $scope.song.markPalette(palette);
+      });
     }, 100);
 
+    $scope.curSong = function () {
+      return mbPlayerEngine.getCurrentSong();
+    };
+    $scope.songAction = function (action, query, idx) {
+
+    };
     $scope.orderTopTracks = function (track) {
       var num = parseInt(track.playcount) * (-1);
       return (_.isNaN(num)) ? -1 : num;
     };
+    $scope.artPalette = function () {
+      return $scope.song.palette || $scope.song.getPrevious().palette;
+    };
 
-    function setTrackProgress(idx, percentProgress) {
-      if ($scope.artistTopTracks.track[idx].circleValue === storedPlaysCounts[idx])
-        return; // already loaded.
-      $scope.artistTopTracks.track[idx].circleValue = $scope.artistTopTracks.maxplayscount * percentProgress;
-    }
 
-    function _updateUrlParams(songMetainfos) {
-      console.log('[Discover] Update url paramss');
-      var anyChanges = false;
-      if ($location.search().artist != songMetainfos.artist
-        || $location.search().title != songMetainfos.title
-        || $location.search().album != songMetainfos.album)
-        anyChanges = true;
-
-      $location.search('artist', songMetainfos.artist);
-      $location.search('title', songMetainfos.title);
-      $location.search('album', songMetainfos.album);
-
-      if (anyChanges)
-        $location.replace();
-    }
-
-    function _buildParamsSongPlaceholder() {
-      if ($location.search().artist) {
-        return {metainfos: _.pick($location.search(), 'artist', 'album', 'title')};
-      } else
-        return {};
-    }
-
-    $scope.showScrollbar = function() {
+    $scope.showScrollbar = function () {
       return ($scope.artistScroll == true && $scope.mobileView == false);
-    }
+    };
     $scope.mobileView = false;
-    $scope.$watch(function(){
+    $scope.$watch(function () {
       return $window.innerWidth;
-    }, function(value) {
-      $scope.mobileView = (value <= 720);
+    }, function (value) {
+      $scope.mobileView = (value <= 990);
     });
   });
