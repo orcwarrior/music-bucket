a212fade4ae9876d55a35b5e0fba57f260961e22
orcwarrior <orcwarrior@o2.pl>
2016-01-13 05:27:47 +0100

    playlist mongoose model: added _base

--- server/api/playlist/playlist.model.js
+++ server/api/playlist/playlist.model.js
@@ -1,24 +1,25 @@
 'use strict';
 
 var mongoose = require('mongoose'),
-    Schema   = mongoose.Schema;
+  Schema = mongoose.Schema;
 
 var PlaylistSchema = new Schema({
-                                  name       : String,
-                                  author     : {
-                                    type: Schema.ObjectId,
-                                    ref : 'User'
-                                  },
-                                  authorName : String,
-                                  description: String,
-                                  entries    : Array,
-                                  songsCount : Number,
-                                  sampleSongs: Array,
-                                  visibility: String,
-                                  tags : Array,
-                                  imageUrl: String,
-                                  modified: { type: Date, default: Date.now },
-                                  totalPlaytime: { type: Number, default: 0 }
-                                });
+  name: String,
+  author: {
+    type: Schema.ObjectId,
+    ref: 'User'
+  },
+  _base: String,
+  authorName: String,
+  description: String,
+  entries: Array,
+  songsCount: Number,
+  sampleSongs: Array,
+  visibility: String,
+  tags: Array,
+  imageUrl: String,
+  modified: {type: Date, default: Date.now},
+  totalPlaytime: {type: Number, default: 0}
+});
 
 module.exports = mongoose.model('Playlist', PlaylistSchema);
