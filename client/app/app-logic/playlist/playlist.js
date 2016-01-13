69431e69c301a329da8d416788021000255d1b5d
orcwarrior <orcwarrior@o2.pl>
2016-01-13 04:57:49 +0100

    Playlist: sequencer events firing and fix with playlist sample songs img alternation of playlist, + __models__

--- client/app/app-logic/playlist/playlist.js
+++ client/app/app-logic/playlist/playlist.js
@@ -21,15 +21,16 @@
             src: song.metainfos.albumArt,
             description: song.metainfos.getSongDescription()
           };
-          playlistService.save(playlist);
+          // if (playlistService.isPlaylistOwner(playlist))
+          //   playlistService.save(playlist);
         }
         else {
           return; // break from storing again in db
         };
       }
 
-      return function playlist (base) {
-          var self = this;
+      var playlistFunc = function playlist(base) {
+        var self = this;
 
         function init() {
           self.name = '';
@@ -37,59 +38,106 @@
           self.description = '';
           self.sampleSongs = [];
           self.isAltered = false;
-        };
+        }
+
         init();
 
-        this.clear = function() { init(); }
+        this.clear = function () {
+          init();
+        }
 
-          this.playlistSequencer = playlistSequencers['random'];
-          this.nextPlaylistSequencer = function () {
-            var playlistSequencersArr = playlistSequencers.toArray();
-            var thisIdx = playlistSequencersArr.indexOf(this.playlistSequencer);
-            var nextIdx = ++thisIdx % playlistSequencersArr.length;
-            this.playlistSequencer = playlistSequencersArr[nextIdx];
-          };
+        this.playlistSequencer = playlistSequencers['random'];
+        this.nextPlaylistSequencer = function () {
+          this.playlistSequencer.unset();
+          var playlistSequencersArr = playlistSequencers.toArray();
+          var thisIdx = playlistSequencersArr.indexOf(this.playlistSequencer);
+          var nextIdx = ++thisIdx % playlistSequencersArr.length;
+          this.playlistSequencer = playlistSequencersArr[nextIdx];
+          this.playlistSequencer.setup();
+        };
 
-          _.extend(this, playlistLocalStorage);
-          _.extend(this, base);
+        _.extend(this, playlistLocalStorage);
+        _.extend(this, base);
 
-          // methods:
-          this.isEmpty = function () { return !this.entries.length; };
-          this.getNext = function () {
-            return this.playlistSequencer.getNext(this.entries, this.songsCount, { playlistCallback: this.getNextCallback });
-            /* an promise*/
-          };
-          this.getNextCallback = function (song) {
-            putSongToSampler(self, song);
-          };
-          this.addEntry = function (entry) {
-            this.entries.push(entry);
-            this.alter();
-          };
-          this.removeEntry = function (entryId) {
-            this.entries = _.reject(this.entries, function (entry) {
-              return entry.id == entryId;
-            });
-            this.alter();
-          };
+        // methods:
+        this.isEmpty = function () {
+          return !this.entries.length;
+        };
+        this.getNext = function () {
+          return this.playlistSequencer.getNext(this.entries, this.songsCount, {playlistCallback: this.getNextCallback});
+          /* an promise*/
+        };
+        this.getNextCallback = function (song) {
+          putSongToSampler(self, song);
+        };
+        this.addEntry = function (entry) {
+          this.entries.push(entry);
+          this.alter();
+        };
+        this.removeEntry = function (entryId) {
+          this.entries = _.reject(this.entries, function (entry) {
+            return entry.id == entryId;
+          });
+          this.alter();
+        };
 
-          this.alter = function () {
-            this.isAltered = true;
-            this.modified = new Date();
-            this.sampleSongs.splice(Math.round(this.sampleSongs.length * Math.random()), 1);
-            this.recalculateSongsCount();
-            this.storeInLocalstorage();
-          };
-          this.recalculateSongsCount = function () {
-            this.songsCount = 0;
-            _.each(this.entries, function (entry) {
-              if (!_.isUndefined(entry.songsCount))
-                self.songsCount += entry.songsCount;
-              else
-                self.songsCount++;
-            });
-          };
-          return this;
+        this.alter = function () {
+          this.isAltered = true;
+          this.modified = new Date();
+          this.sampleSongs.splice(Math.round(this.sampleSongs.length * Math.random()), 1);
+          this.recalculateSongsCount();
+          this.storeInLocalstorage();
+        };
+        this.recalculateSongsCount = function () {
+          this.songsCount = 0;
+          _.each(this.entries, function (entry) {
+            if (!_.isUndefined(entry.songsCount))
+              self.songsCount += entry.songsCount;
+            else
+              self.songsCount++;
+          });
+        };
+        return this;
       };
+
+      playlistFunc.prototype.__models__ = {
+        db: {
+          base: "playlist",
+          pickedFields: [
+            'id',
+            'author',
+            'authorName',
+            'name',
+            'songsCount',
+            'sampleSongs',
+            'visibility',
+            'tags',
+            'imageUrl',
+            'modified',
+            'description',
+            'entries']
+        },
+        cookies: {
+          base: "playlist",
+          pickedFields: [
+            'id',
+            'author',
+            'authorName',
+            'name',
+            'songsCount',
+            'sampleSongs',
+            'visibility',
+            'tags',
+            'imageUrl',
+            'modified',
+            'description',
+            'playlistSequence',
+            'isAltered',
+            'entries'
+          ]
+        }
+      };
+
+      return playlistFunc;
     })
 })();
