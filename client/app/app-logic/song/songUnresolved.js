25877f208758205799969ddf74c8d530b06683c2
orcwarrior <orcwarrior@o2.pl>
2016-01-13 05:14:55 +0100

    songUnresolved: resolve function impl (+ default) and __models__

--- client/app/app-logic/song/songUnresolved.js
+++ client/app/app-logic/song/songUnresolved.js
@@ -4,18 +4,63 @@
 
 (function () {
   angular.module('musicBucketEngine')
-    .factory('songUnresolved', function (songControllsInterface) {
-      var songUnresolved = function(response, type, resolveFunction) {
-        this.resolve = _.bind(resolveFunction, this, response, type)
-        this.response = response;
-        this.type = type;
+    .factory('songUnresolved', function (songControllsInterface, songCommons, songSeeker, songMetainfos, hashGenerator) {
 
+      var defaultResolveFunction = function (initData, resolveCb) {
+        var self = this;
+        new songSeeker(initData, true)
+          .then(function (song) {
+            song.entryId = self.entryId; // rewrite old entryId;
+            self = song;
+            //_.extendOwn(self, song);
+            self.resolve = undefined;
+            if (!_.isUndefined(resolveCb))
+              resolveCb(self);
+          });
+      };
+
+      var songUnresolved = function songUnresolved (initData, resolveFunction, entryId) {
+        this.id = hashGenerator.generateId();
+        this.resolveFunction = resolveFunction;
+        this.initData = initData;
+        this.resolve = _.bind(this.resolveFunction || defaultResolveFunction, this, initData || this.initData);
+        this.entryId = entryId;
+        this.type = songCommons.songType.unresolved;
+
+        if (initData instanceof songMetainfos)
+          this.metainfos = initData;
       }
-      songUnresolved.prototype = new songControllsInterface(function() {
-        console.log("songUnresolved controlls called");
-        console.log(_.last(arguments));
-         this.resolve();
+      songUnresolved.prototype = new songControllsInterface(function () {
+        if (this.type === songCommons.songType.unresolved) {
+          console.log("songUnresolved controlls called !!!");
+          console.log(_.last(arguments));
+          this.resolve();
+        } else {
+
+        }
       });
+      songUnresolved.prototype.__models__ = {
+        db: {
+          base: "songUnresolved",
+          constructorArgs : ['initData', 'resolveFunction', 'entryId'],
+          pickedFields: [
+            'id',
+            'resolveFunction',
+            'entryId',
+            'type',
+            'initData']
+        },
+        cookies: {
+          base: "songUnresolved",
+          constructorArgs : ['initData', 'resolveFunction', 'entryId'],
+          pickedFields: [
+            'id',
+            'resolveFunction',
+            'entryId',
+            'type',
+            'initData']
+        }
+      };
 
       return songUnresolved;
     });
