6ddb7b4e26f1c59450fcf2af5dcf72da0e9b164c
orcwarrior <orcwarrior@o2.pl>
2016-01-13 05:08:04 +0100

    main.intro: added lurker stats and stuff.

--- client/app/routes/main.intro/main.intro.controller.js
+++ client/app/routes/main.intro/main.intro.controller.js
@@ -1,6 +1,11 @@
 'use strict';
 
 angular.module('musicBucketApp')
-  .controller('MainIntroCtrl', function ($scope) {
+  .controller('MainIntroCtrl', function ($scope, $http) {
     $scope.windLoc = window.location.host;
+
+    $http.get('/api/songzaLurker/stats')
+      .then(function (response) {
+        $scope.songzaLurkerStats = response.data;
+      })
   });
