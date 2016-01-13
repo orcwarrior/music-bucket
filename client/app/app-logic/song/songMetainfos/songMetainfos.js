caa58ea441f268ce354b4f8f437265a2e501a82a
orcwarrior <orcwarrior@o2.pl>
2016-01-13 05:20:23 +0100

    songMetainfos: added __models__

--- client/app/app-logic/song/songMetainfos/songMetainfos.js
+++ client/app/app-logic/song/songMetainfos/songMetainfos.js
@@ -5,22 +5,55 @@
 (function () {
   angular.module('musicBucketEngine')
     .factory('songMetainfos', function () {
-      return function songMetainfos() {
+      var songMetainfosFunc = function songMetainfos(srcMetainfos) {
         this.id = undefined;
         this.artist = "";
         this.title = "";
         this.album = undefined;
         this.albumArt = undefined;
         this.genere = "";
-        this.getSrc = function () { return this.url; };
+        this.getSrc = function () {
+          return this.url;
+        };
         this.getSongDescription = function () {
-            if (this.title === '')
-              return this.id;
-            if (this.artist === '')
-              return this.title;
-            else return this.artist + ' - ' + this.title;
-          }
+          if (this.title === '')
+            return this.id;
+          if (this.artist === '')
+            return this.title;
+          else return this.artist + ' - ' + this.title;
+        }
+
+        if (!_.isUndefined(srcMetainfos)) {
+          _.extendOwn(this, srcMetainfos);
+        }
       }
+
+      songMetainfosFunc.prototype.__models__ = {
+        db: {
+          base: "songMetainfos",
+          pickedFields: [
+            'id',
+            'artist',
+            'title',
+            'album',
+            'albumArt',
+            'genere',
+            'url']
+        },
+        cookies: {
+          base: "songMetainfos",
+          pickedFields: [
+            'id',
+            'artist',
+            'title',
+            'album',
+            'albumArt',
+            'genere',
+            'url']
+        }
+      };
+
+      return songMetainfosFunc;
 
     });
 })();
