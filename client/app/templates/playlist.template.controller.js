13509acb1f3d7fa47ab80f3804b682b4ee47b449
orcwarrior <orcwarrior@o2.pl>
2016-01-13 04:59:05 +0100

    Playlist template ctrl: Error fix ($scope.playlist)

--- client/app/templates/playlist.template.controller.js
+++ client/app/templates/playlist.template.controller.js
@@ -3,27 +3,28 @@
  */
 
 angular.module('musicBucketApp')
-.controller('PlaylistSaveDialogController', function ($mdDialog, $scope, playlist, playlistService) {
+  .controller('PlaylistSaveDialogController', function ($mdDialog, $scope, playlist, playlistService) {
 
-  if (_.isUndefined(playlist.tags))
-    playlist.tags = [];
+    $scope.playlist = playlist;
+    if (_.isUndefined(playlist.tags))
+      playlist.tags = [];
 
-  $scope.coverEditingIcon = 'insert_photo';
-  $scope.acceptMsg = "Save";
+    $scope.coverEditingIcon = 'insert_photo';
+    $scope.acceptMsg = "Save";
 
-  $scope.toggleCoverEditing = function () {
-    $scope.coverEditing = !$scope.coverEditing;
-    $scope.coverEditingIcon = ($scope.coverEditing) ? 'done' : 'insert_photo';
-  }
-  $scope.hide = function () {
-    $mdDialog.hide();
-  };
-  $scope.cancel = function () {
-    $mdDialog.cancel();
-  };
-  $scope.answer = function (answer) {
-    if (answer === 'save')
-      playlistService.save($scope.playlist);
-    $mdDialog.hide(answer);
-  }
-});
+    $scope.toggleCoverEditing = function () {
+      $scope.coverEditing = !$scope.coverEditing;
+      $scope.coverEditingIcon = ($scope.coverEditing) ? 'done' : 'insert_photo';
+    }
+    $scope.hide = function () {
+      $mdDialog.hide();
+    };
+    $scope.cancel = function () {
+      $mdDialog.cancel();
+    };
+    $scope.answer = function (answer) {
+      if (answer === 'save')
+        playlistService.save($scope.playlist);
+      $mdDialog.hide(answer);
+    }
+  });
