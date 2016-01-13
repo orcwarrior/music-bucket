3877a20088c53b67cbbfe4f4f5e88580952d45cb
orcwarrior <orcwarrior@o2.pl>
2016-01-13 04:25:14 +0100

    entryCommons: new virtualEntry type, and entry sequenceTypes (handy for album based entry types - to be played from begining to end)

--- client/app/app-logic/playlist/entry/entryCommons.js
+++ client/app/app-logic/playlist/entry/entryCommons.js
@@ -1,18 +1,19 @@
 /**
  * Created by orcwarrior on 2014-12-26.
  */
-
-
 (function () {
   angular.module('musicBucketEngine')
     .factory('entryCommons', function () {
-
-               return {
-                 entryType    : {local: "eLocal", songzaPlaylist: "eSongzaPlaylist", youtubeVideo: "eYoutubeVideo", youtubePlaylist: "eYoutubePlaylist", soundcloudTrack: 'eSoundcloudTrack' /*...*/},
-                 getPlaylistDescription : function () {
-                   return this.shortDescription;
-                 }
-               }
-             });
-}
-)();
+      return {
+        nextOrder: {sequence: 'eOrderSeq', random: 'eOrderRand'},
+        entryType: {
+          local: "eLocal",
+          songzaPlaylist: "eSongzaPlaylist",
+          youtubeVideo: "eYoutubeVideo",
+          youtubePlaylist: "eYoutubePlaylist",
+          soundcloudTrack: 'eSoundcloudTrack',
+          virtalPlaylist: "eVirtual" /*...*/
+        }
+      }
+    });
+})();
