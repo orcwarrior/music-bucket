4db1dbe51d171e3eb870e79685bf5f529a9a8ee1
orcwarrior <orcwarrior@o2.pl>
2016-02-21 00:35:15 +0100

    mbPlayerListContainer

---
+++ client/app/mbPlayerListContainer/mbPlayerListContainer.directive.js
@@ -0,0 +1,27 @@
+'use strict';
+
+angular.module('musicBucketApp')
+  .directive('mbPlayerListContainer', function ($rootScope, $timeout) {
+    return {
+      templateUrl: 'app/mbPlayerListContainer/mbPlayerListContainer.html',
+      restrict: 'EAC',
+      scope: {
+        container : "=container"
+      },
+      link: function (scope, element, attrs) {
+        scope.actionsToggled = [];
+        scope.toggleActions = function ($index) {
+          var oldState = !!scope.actionsToggled[$index];
+          scope.actionsToggled = [];
+          scope.actionsToggled[$index] = !oldState;
+        };
+        scope.$watch('container', function() {
+          $rootScope.$broadcast('list-scroll:update', null);
+          $timeout(function() {
+            var container = angular.element(element[0].parentElement);
+            $rootScope.$broadcast('list-scroll:update', null);
+          }, 1000, true);
+        });
+      }
+    };
+  });
