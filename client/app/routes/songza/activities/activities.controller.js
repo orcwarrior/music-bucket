db711117cdcca6b129d8592468b151571e6447d5
orcwarrior <orcwarrior@o2.pl>
2016-02-21 00:40:59 +0100

    songza views adapted to new lurker api (2)

--- client/app/routes/songza/activities/activities.controller.js
+++ client/app/routes/songza/activities/activities.controller.js
@@ -4,32 +4,31 @@ angular.module('musicBucketApp')
   .controller('SongzaActivitiesCtrl', function ($scope, $location, songzaApi) {
     $scope.grouping = "activity";
     if (_.isUndefined($scope.activities))
-    songzaApi.activities()
-      .then(function (activities) {
-        $scope.activities = activities.data;
-        if ($location.search().slug) {
-          $scope.selectedActivity = _.find($scope.activities, function (act) {
-            return act.slug === $location.search().slug;
-          });
-          if (!_.isUndefined($scope.selectedActivity))
-            loadActivityStations();
-        }
-      });
+      songzaApi.activities()
+        .then(function (activities) {
+          $scope.activities = activities.data;
+          if ($location.search().slug) {
+            $scope.selectedActivity = _.find($scope.activities, function (act) {
+              return act.slug === $location.search().slug;
+            });
+            if (!_.isUndefined($scope.selectedActivity))
+              loadActivityStations();
+          }
+        });
     function loadActivityStations() {
       $scope.stations = null;
-      songzaApi.station.multi($scope.selectedActivity.station_ids)
+      songzaApi.station.multi($scope.selectedActivity.stations)
         .then(function (response) {
           $scope.stations = response.data;
           $location.replace();
         });
 
     }
+
     $scope.setActivity = function (activity) {
       $scope.selectedActivity = activity;
       $location.search('slug', activity.slug);
       $location.replace();
       loadActivityStations();
     };
-
-
   });
