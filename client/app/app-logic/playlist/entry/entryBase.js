5e5a1130931bc9e723a75080185dd41329afc493
orcwarrior <orcwarrior@o2.pl>
2016-02-21 00:21:35 +0100

    entryBase: added isActive, removeSong and resolve

--- client/app/app-logic/playlist/entry/entryBase.js
+++ client/app/app-logic/playlist/entry/entryBase.js
@@ -4,7 +4,7 @@
 
 (function () {
   angular.module('musicBucketEngine')
-    .factory('entryBase', function () {
+    .factory('entryBase', function ($injector) {
 
       var entryBase = function entryBase() {
       };
@@ -30,9 +30,32 @@
         resetPlayedSongs: function () {
           if (this.playedCount) this.playedCount = 0;
           if (this.playedIDs) this.playedIDs = [];
-          if (this.nonPlayedSongs) this.nonPlayedSongs = _.map(this.entries, function(e) {return e.id; });
+          // if (this.nonPlayedSongs) this.nonPlayedSongs = _.map(this.entries, function (e) {
+          //   return e.id;
+          // });
+        },
+        isActive: function () {
+          // TODO: REFATOR!!!
+          var engine = $injector.get('mbPlayerEngine');
+          var song = engine.getCurrentSong();
+          return !_.isUndefined(song) && this.id == song.entryId;
+        },
+        getName: function () {
+          // Check if there's custon entry name too
+          if (this._name)
+            return this._name + " (" + this.getPlayedCount() + "/" + this.getSongsCount() + ")";
+          else
+            return this.getPlaylistDescription();
+        },
+        removeSong: function(song) {
+          this.entries = _.reject(this.entries, _.matcher(song));
+          if (this.playedIDs) {
+            var altered = _.without(this.playedIDs, song);
+            if (altered.length !== this.playedIDs.length)
+              this.playedCount--;
+            this.playedIDs = altered;
+          }
         }
-
       };
       return entryBase;
     });
