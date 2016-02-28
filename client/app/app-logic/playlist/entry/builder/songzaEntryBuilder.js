b8bd8010823b22903bd3cd77335e4fc43eea420d
orcwarrior <orcwarrior@o2.pl>
2016-02-28 18:47:51 +0100

    songzaEntryBuilder

---
+++ client/app/app-logic/playlist/entry/builder/songzaEntryBuilder.js
@@ -0,0 +1,59 @@
+/**
+ * Created by orcwa on 15.11.2015.
+ */
+
+(function () {
+  angular.module('musicBucketEngine')
+    .factory('songzaEntryBuilder', function ($q, $injector, songzaApi, entryCommons, virtualEntry, songMetainfos, songUnresolved, entryBuilder) {
+      function _hasFullSongzaInfos(songzaInfos) {
+        return !_.isUndefined(songzaInfos.songs) && songzaInfos.songs.length && _.isObject(songzaInfos.songs[0]);
+      }
+
+      function _fetchFullSongzaInfos(songzaInfos) {
+        if (_.isObject(songzaInfos)) {
+          return songzaApi.station.get(songzaInfos._id);
+        } else {
+          return songzaApi.station.get(songzaInfos);
+        }
+      }
+
+      function buildBasedOnStation(songzaStation, restoredObj) {
+        var songzaEntry = new virtualEntry(songzaStation.name, undefined, entryCommons.nextOrder.random);
+        _.each(songzaStation.songs, function (song) {
+          songzaEntry.addSong(new songUnresolved(
+            new songMetainfos({
+              artist: song.artist,
+              album: song.album,
+              title: song.title
+              //albumArt: song.albumArt,
+              //trackNo: song.trackNo
+            })
+            /*,resolveFunction (use default)*/
+          ));
+        });
+        if (restoredObj)
+          _.extendOwn(songzaEntry, restoredObj);
+        // setup builder
+        songzaEntry.__builder__ = {name: 'songzaEntryBuilder', data: songzaStation};
+        return songzaEntry;
+      }
+
+      var songzaEntryBuilderFunc = function songzaEntryBuilder() {
+        this.build = function (songzaInfos, restoredObj) {
+          var deffered = $q.defer();
+          if (_hasFullSongzaInfos(songzaInfos))
+            deffered.resolve(buildBasedOnStation(songzaInfos, restoredObj));
+          else {
+            _fetchFullSongzaInfos(songzaInfos, restoredObj)
+              .then(function (res) {
+                deffered.resolve(buildBasedOnStation(res.data, restoredObj));
+              });
+          }
+          return deffered.promise;
+        };
+      };
+      songzaEntryBuilderFunc.prototype = entryBuilder;
+
+      return new songzaEntryBuilderFunc();
+    });
+})();
