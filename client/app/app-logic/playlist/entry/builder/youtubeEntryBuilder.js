e97087d6277debc1907a59a455bbffb77b03205c
orcwarrior <orcwarrior@o2.pl>
2016-02-28 18:51:17 +0100

    ytEntryBuilder: uses promise as build return, wait's for entry to fully create before diffing and resolving with stored obj.

--- client/app/app-logic/playlist/entry/builder/youtubeEntryBuilder.js
+++ client/app/app-logic/playlist/entry/builder/youtubeEntryBuilder.js
@@ -4,7 +4,7 @@
 
 (function () {
   angular.module('musicBucketEngine')
-    .factory('youtubeEntryBuilder', function ($injector, entryCommons, songCommons, youtubePlaylistEntry, virtualEntry, song) {
+    .factory('youtubeEntryBuilder', function ($q, $injector, entryCommons, songCommons, youtubePlaylistEntry, virtualEntry, song, entryBuilder) {
 
       // NOTE: Strange bug, regex instances cannot be again matched:
       //var playlistRegex = /[?&]list=([\S?&]+)($|[?&])/g;
@@ -31,34 +31,50 @@
       function isYoutubePlaylist(entry) {
         if (_.isObject(entry)) {
           return (entry.kind !== "youtube#video");
+        } else {
+          return entry.match(playlistRegex()) !== null; // no playlist string, so it's single video
         }
-        return entry.match(playlistRegex()) !== null; // no playlist string, so it's single video
       }
+
       const ytVirtualEntryName = "Youtube Videos";
+
       function getYTVideosVirtualEntry() {
         var mbPlayerEngine = $injector.get('mbPlayerEngine');
         var curPlaylist = mbPlayerEngine.getPlaylist();
         if (curPlaylist) {
-          var ytVideosEntries = curPlaylist.findEntry({id: 'VIR-'+ytVirtualEntryName});
+          var ytVideosEntries = curPlaylist.findEntry({id: 'VIR-' + ytVirtualEntryName});
           if (_.isUndefined(ytVideosEntries)) {
-            ytVideosEntries = new virtualEntry(ytVirtualEntryName,[], entryCommons.nextOrder.random)
+            ytVideosEntries = new virtualEntry(ytVirtualEntryName, undefined, entryCommons.nextOrder.random);
+            // it's need to be added (shouldn't be done actually here)
           }
           return ytVideosEntries;
         }
       }
 
-      var youtubeEntryBuilderFunc = function youtubeEntryBuilder(entry) {
-          if (isYoutubePlaylist(entry)) {
-            return new youtubePlaylistEntry(entry);
+      var youtubeEntryBuilder = function albumEntryBuilder() {
+        this.build = function (infos, restoredObj) {
+          var deffered = $q.defer();
+          if (isYoutubePlaylist(infos)) {
+            var ytPlaylist = new youtubePlaylistEntry(infos);
+
+            ytPlaylist.__entriesPromise.then(function(resolvedEntries) {
+              if (restoredObj)
+                ytPlaylist = _.deepExtend(ytPlaylist, restoredObj);
+              // setup builder
+              ytPlaylist.__builder__ = {name: 'youtubeEntryBuilder', data: infos};
+              deffered.resolve(ytPlaylist);
+            });
           } else {
             var virtualEntry = getYTVideosVirtualEntry();
-            var ytId = entry.videoId || extractIdFromUrl(entry);
+            var ytId = infos.videoId || extractIdFromUrl(infos);
             virtualEntry.addSong(new song(ytId, songCommons.songType.youtube, ytId));
             // this.entries = [new song(this.id, songCommons.songType.youtube, this.id)];
-            return virtualEntry;
+            deffered.resolve(virtualEntry);
           }
+          return deffered.promise;
+        };
       };
-
-      return youtubeEntryBuilderFunc;
+      youtubeEntryBuilder.prototype = entryBuilder;
+      return new youtubeEntryBuilder();
     });
 })();
