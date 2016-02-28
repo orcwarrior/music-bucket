a8361e612b8877aa9d8f28451377d4405560acfd
orcwarrior <orcwarrior@o2.pl>
2016-02-28 19:02:47 +0100

    playlistLocalStorage: refactoring, promise based, loading of LSPlaylist fixed too (main.ctrl)

--- client/app/app-logic/playlist/playlistLocalStorage.js
+++ client/app/app-logic/playlist/playlistLocalStorage.js
@@ -3,39 +3,46 @@
  */
 (function () {
   angular.module('musicBucketEngine')
-    .factory('playlistLocalStorage', function (playlistCookieFactory, localStorageService) {
+    .factory('playlistLocalStorage', function ($q, modelConverter, localStorageService) {
 
-                const localStorageVersion = "0.1.5";
-                // WEAK AS FUCK, REFACTOR LATER!
-                 function storeInLocalstorage(playlist) {
-                   localStorageService.remove('playlist');
-                   var lsPlaylist = playlistCookieFactory.convertTo(playlist)
-                   lsPlaylist.version = localStorageVersion;
-                   localStorageService.set('playlist', JSON.stringify(lsPlaylist));
-                 }
+      const localStorageVersion = "0.2.1";
+      // WEAK AS FUCK, REFACTOR LATER!
 
-                 function restoreFromLocalstorage() {
-                   var lsPlaylist = localStorageService.get('playlist');
-                   if (_.isNull(lsPlaylist)) return null;
-                   if (lsPlaylist.version !== localStorageVersion) {
-                     localStorageService.remove('playlist');
-                     alert("There was local storage version update, all unsaved playlist changes will be lost!");
-                     return null;
-                   }
+      return {
+        restoreFromLocalstorage: function () {
+          var deffered = $q.defer();
+          var lsPlaylist = localStorageService.get('playlist');
+          if (_.isNull(lsPlaylist)) {
+            deffered.reject({err: "There is no localStorage playlists."});
+            return deffered.promise;
+          }
 
-                   var playlist = playlistCookieFactory.convertFrom(lsPlaylist);
-                   return playlist;
-                 }
-
-                 return {
-                   restoreFromLocalstorage : function () {
-                     var lsPlaylist = restoreFromLocalstorage();
-                     return lsPlaylist;
-                   },
-                   storeInLocalstorage : function () {
-                     storeInLocalstorage(this);
-                     //storeInLocalstorage(this);
-                   }
-                }
-               })
-  })();
+          if (lsPlaylist.version !== localStorageVersion) {
+            localStorageService.remove('playlist');
+            alert("There was local storage version update, all unsaved playlist changes will be lost!");
+            deffered.reject({err: "There was local storage version update, all unsaved playlist changes will be lost!"});
+            deffered.reject({err: "There was local storage version update, all unsaved playlist changes will be lost!"});
+            return deffered.promise;
+          }
+          modelConverter.convertFromModel(lsPlaylist, 'cookies')
+            .then(function (lsPlaylist) {
+              console.log("Converted playlist:");
+              console.log(lsPlaylist);
+              lsPlaylist.version = localStorageVersion;
+              return deffered.resolve(lsPlaylist);
+            });
+          return deffered.promise;
+        },
+        storeInLocalstorage: function () {
+          localStorageService.remove('playlist');
+          modelConverter.convertToModel(this, 'cookies')
+            .then(function (lsPlaylist) {
+              console.log("Converted playlist:");
+              console.log(lsPlaylist);
+              lsPlaylist.version = localStorageVersion;
+              localStorageService.set('playlist', JSON.stringify(lsPlaylist));
+            });
+        }
+      }
+    })
+})();
--- client/app/routes/main/main.controller.js
+++ client/app/routes/main/main.controller.js
@@ -2,91 +2,97 @@
 
 angular.module('musicBucketApp')
   .controller('MainCtrl',
-              function ($rootScope, $animate, $scope, $http, $location, socket, Auth, mbPlayerEngine, playlistLocalStorage, songCommons, playlistService, playlist, hotkeys) {
-                var autoplay = $location.search().autoplay;
-                var theaterMode = $location.search().theater;
-                // Loading playlist from cookies:
-                // TODO#2: Playlist should be FULLY recreated by LSProvider
-                // TODO#3: Having this mechanics here is fuckedup
-                soundManager.onready(function() {
-                  if (autoplay) return; // BUGFIX: When autoplay argument is active, don't load LS playlist!
-                  var lsPlaylist = playlistLocalStorage.restoreFromLocalstorage();
-                  if (!_.isNull(lsPlaylist)) {
-                    console.log("LS Playlist:"); console.log(lsPlaylist);
-                    mbPlayerEngine.setPlaylist(new playlist(lsPlaylist));
-                    $scope.$broadcast('list-scroll:update', null);
-                  }
-                });
+  function ($rootScope, $animate, $scope, $http, $location, socket, Auth, mbPlayerEngine, playlistLocalStorage, songCommons, playlistService, playlist, hotkeys) {
+    var autoplay = $location.search().autoplay;
+    var theaterMode = $location.search().theater;
+    // Loading playlist from cookies:
+    // TODO#2: Playlist should be FULLY recreated by LSProvider
+    // TODO#3: Having this mechanics here is fuckedup
+    soundManager.onready(function () {
+      if (autoplay) return; // BUGFIX: When autoplay argument is active, don't load LS playlist!
+      playlistLocalStorage.restoreFromLocalstorage()
+        .then(function (lsPlaylist) {
+          if (!lsPlaylist.err) {
+            console.log("LS Playlist:");
+            console.log(lsPlaylist);
+            var restoredPlaylist = new playlist(lsPlaylist);
+            mbPlayerEngine.setPlaylist(restoredPlaylist);
+            $scope.$broadcast('playlist:update', restoredPlaylist);
+            $scope.$broadcast('list-scroll:update', restoredPlaylist);
+          }
+        })
+    });
 
-                $scope.userLoggedIn = function () {
-                  return Auth.isLoggedIn();
-                }
-                $rootScope.player = mbPlayerEngine;
+    $scope.userLoggedIn = function () {
+      return Auth.isLoggedIn();
+    }
+    $rootScope.player = mbPlayerEngine;
 
-                // Playlist items dropdowns:
-                $scope.playlistItemDropdown = false;
-                $scope.togglePlaylistItemDropdown = function ($event) {
-                  $event.preventDefault();
-                  $event.stopPropagation();
-                  $scope.status.isopen = !$scope.status.isopen;
-                };
-                // New track playing: (store playlist in LS)
-                $scope.$on('track:id', function (event, data) {
-                  mbPlayerEngine.getPlaylist().storeInLocalstorage();
-                });
-                $scope.toggleTeatherMode = function() {
-                  mbPlayerEngine.theaterMode.enabled = !mbPlayerEngine.theaterMode.enabled;
-                  $scope.$broadcast("theater:mode", mbPlayerEngine.theaterMode.enabled);
-                  $scope.$broadcast('list-scroll:update', null);
-                };
+    // Playlist items dropdowns:
+    $scope.playlistItemDropdown = false;
+    $scope.togglePlaylistItemDropdown = function ($event) {
+      $event.preventDefault();
+      $event.stopPropagation();
+      $scope.status.isopen = !$scope.status.isopen;
+    };
+    // New track playing: (store playlist in LS)
+    $scope.$on('track:id', function (event, data) {
+      mbPlayerEngine.getPlaylist().storeInLocalstorage();
+    });
+    $scope.toggleTeatherMode = function () {
+      mbPlayerEngine.theaterMode.enabled = !mbPlayerEngine.theaterMode.enabled;
+      $scope.$broadcast("theater:mode", mbPlayerEngine.theaterMode.enabled);
+      $scope.$broadcast('list-scroll:update', null);
+    };
 
-                // Initialize hotkeys:
-                hotkeys.bindTo($rootScope)
-                  .add({
-                    combo: ['space', 'stop-playing'],
-                    description: 'Toggle playing',
-                    callback: _.bind(mbPlayerEngine.togglePlay, mbPlayerEngine)
-                  })
-                  .add({
-                    combo: ['mod+up','next-track'],
-                    description: 'Play next track',
-                    action: 'keydown',
-                    callback: _.bind(mbPlayerEngine.nextTrack, mbPlayerEngine)
-                  })
-                  .add({
-                    combo: ['mod+down','prev-track'],
-                    description: 'Rewind to previous track',
-                    action: 'keydown',
-                    callback: _.bind(mbPlayerEngine.prevTrack, mbPlayerEngine)
-                  })
-                  .add({
-                  combo: ['t'],
-                  description: 'Toggle Theater mode',
-                  action: 'keydown',
-                  callback: $scope.toggleTeatherMode});
+    // Initialize hotkeys:
+    hotkeys.bindTo($rootScope)
+      .add({
+        combo: ['space', 'stop-playing'],
+        description: 'Toggle playing',
+        callback: _.bind(mbPlayerEngine.togglePlay, mbPlayerEngine)
+      })
+      .add({
+        combo: ['mod+up', 'next-track'],
+        description: 'Play next track',
+        action: 'keydown',
+        callback: _.bind(mbPlayerEngine.nextTrack, mbPlayerEngine)
+      })
+      .add({
+        combo: ['mod+down', 'prev-track'],
+        description: 'Rewind to previous track',
+        action: 'keydown',
+        callback: _.bind(mbPlayerEngine.prevTrack, mbPlayerEngine)
+      })
+      .add({
+        combo: ['t'],
+        description: 'Toggle Theater mode',
+        action: 'keydown',
+        callback: $scope.toggleTeatherMode
+      });
 
-                // User Idle in theater mode:
-                $scope.$on('IdleStart', function() {
-                  // the user appears to have gone idle
-                  console.log("IDLE START!");
-                  if (mbPlayerEngine.theaterMode.enabled) {
-                    //mbPlayerEngine.theaterMode.playlistMenuToggled = false;
-                    mbPlayerEngine.theaterMode.userIdle = true;
-                  }
-                });
+    // User Idle in theater mode:
+    $scope.$on('IdleStart', function () {
+      // the user appears to have gone idle
+      console.log("IDLE START!");
+      if (mbPlayerEngine.theaterMode.enabled) {
+        //mbPlayerEngine.theaterMode.playlistMenuToggled = false;
+        mbPlayerEngine.theaterMode.userIdle = true;
+      }
+    });
 
-                $scope.$on('IdleEnd', function() {
-                  // the user has come back from AFK and is doing stuff. if you are warning them, you can use this to hide the dialog
-                  $scope.$apply(function() {
-                    mbPlayerEngine.theaterMode.userIdle = false;
-                  });
-                  console.log("IDLE END!");
-                });
+    $scope.$on('IdleEnd', function () {
+      // the user has come back from AFK and is doing stuff. if you are warning them, you can use this to hide the dialog
+      $scope.$apply(function () {
+        mbPlayerEngine.theaterMode.userIdle = false;
+      });
+      console.log("IDLE END!");
+    });
 
-                if (!_.isUndefined(theaterMode)) {
-                  mbPlayerEngine.theaterMode.enabled = true
-                  $scope.$broadcast("theater:mode", mbPlayerEngine.theaterMode.enabled);
-                  $scope.$broadcast('list-scroll:update', null);
-                }
+    if (!_.isUndefined(theaterMode)) {
+      mbPlayerEngine.theaterMode.enabled = true
+      $scope.$broadcast("theater:mode", mbPlayerEngine.theaterMode.enabled);
+      $scope.$broadcast('list-scroll:update', null);
+    }
 
-              });
+  });
