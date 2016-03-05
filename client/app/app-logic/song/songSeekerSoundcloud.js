dbac1e7c81f1dc3c8f8e20b108bda49d30fd8152
orcwarrior <orcwarrior@o2.pl>
2016-03-05 20:42:27 +0100

    songSeekerSoundcloud: added filtering by searched song duration

--- client/app/app-logic/song/songSeekerSoundcloud.js
+++ client/app/app-logic/song/songSeekerSoundcloud.js
@@ -20,15 +20,18 @@ angular.module('musicBucketEngine')
           'entry': entry,
           type: songCommons.songType.soundcloud
         };
-        entryScore.val *= 1.2;
-        entryScore.val += 10;
+        entryScore.val *= 1.15;
+        // entryScore.val += 10;
         // remix fix:
         if (nMetaTitle.indexOf("remix") == -1 && entry.track_type === "remix")
           entryScore.val *= 0.5;
         scoreMap.push(entryScore);
       });
 
-      highestScore = _.chain(scoreMap).sortBy('val').reverse().first().value();
+      highestScore = _.chain(scoreMap).sortBy('val').reverse();
+      console.log("soundcloud scores:");
+      console.log(highestScore);
+      highestScore = highestScore.first().value();
       // TODO: Grab some first entries based on val distribution, get them informations about duration
       // and compare it with metainfos duration, update score
       // additionaly try to
@@ -37,16 +40,18 @@ angular.module('musicBucketEngine')
 
     return function songSeekerSoundcloud(metainfos, searchQuery) {
       var deferred = $q.defer();
-
-      soundcloudApi.search.track(searchQuery, 5)
+      var durationFilter = {};
+      if (metainfos.duration) {
+        durationFilter.getParams = {};
+        durationFilter.getParams['duration[from]'] = (metainfos.duration - 4) * 1000;
+        durationFilter.getParams['duration[to]'] = (metainfos.duration + 4) * 1000;
+      }
+      soundcloudApi.search.track(searchQuery, 5, durationFilter)
         .then(function (response) {
           // TODO: No search results?
           var pickedEntry = _pickSoundcloudEntry(response.data, metainfos);
-          if (pickedEntry) {
-            deferred.resolve(pickedEntry);
-          }
+          deferred.resolve(pickedEntry);
         });
       return deferred.promise;
-
     };
   });
