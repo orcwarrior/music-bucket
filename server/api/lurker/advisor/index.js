5d5624007e96c473000e81afc31e7c74c748363c
orcwarrior <orcwarrior@o2.pl>
2016-03-05 15:41:43 +0100

    api/lurker/advisor: cause of strange unresolveable errors, stations are now populated manually.

--- server/api/lurker/advisor/index.js
+++ server/api/lurker/advisor/index.js
@@ -6,7 +6,27 @@
 var express = require('express');
 var _ = require('lodash');
 var daytimeTargetSchema = require('../../../songzaLurker/models/metaDaytimeTarget.model.js');
+var stationSchema = require('../../../songzaLurker/models/station.model');
 
+function _collectStations(obj) {
+  var flatStations = [];
+  if (!_.isEmpty(obj.stations)) {
+    flatStations.unshift.apply(flatStations, obj.stations);
+  }
+  _.each(obj.situations, function (situation) {
+    flatStations.unshift.apply(flatStations, _collectStations(situation));
+  });
+  return flatStations;
+}
+function _manuallyPopulateStations(obj, stations) {
+  obj.stations = _.map(obj.stations, function (station) {
+    return _.find(stations, _.identity({_id: station}));
+  });
+
+  _.each(obj.situations, function (situation) {
+    _manuallyPopulateStations(situation, stations);
+  });
+}
 module.exports = function (req, res) {
   var day, period;
   if (!req.query.period)
@@ -21,24 +41,25 @@ module.exports = function (req, res) {
   console.log("day: " + day + ", period: " + period);
   var searchQuery = (day) ? daytimeTargetSchema.find({dayOfWeek: day, timeOfDay: period})
     : daytimeTargetSchema.find({timeOfDay: period});
+
   searchQuery
     .populate({
-      path: 'stations', populate: {
-        path: 'situations stations'
-      }
-    })
-    .populate({
       path: 'situations', populate: {
-        path: 'situations stations'
+        path: 'situations' // manual stations propagation needed :<
       }
     })
     .exec(function (err, daytimeTargets) {
-      console.log(daytimeTargets);
-      if (!daytimeTargets.length && req.query.day) {
+      if (err)
+        return console.error(err);
+      if (!daytimeTargets)
+        return console.error("There is no daytimeTargets for given query");
+
+
+      if ((!daytimeTargets || !daytimeTargets.length) && req.query.day) {
         // Try again without concrete day:
         delete req.query.day;
         return module.exports(req, res)
-      } else if (!daytimeTargets.length) {
+      } else if (!daytimeTargets || !daytimeTargets.length) {
         return res.json(404, {err: "this period sitatuions not found"});
       }
       var masterDaytime = _.reduce(daytimeTargets, function (master, cur) {
@@ -49,8 +70,12 @@ module.exports = function (req, res) {
       masterDaytime.stations = _.compact(masterDaytime.stations);
       masterDaytime.situations = _.compact(masterDaytime.situations);
 
-      console.log(masterDaytime);
-      return res.json(200, masterDaytime);
+      var stationsToFetch = _collectStations(masterDaytime);
+      stationSchema
+        .find({'_id': {$in: stationsToFetch}})
+        .exec(function (err, stations) {
+          _manuallyPopulateStations(masterDaytime, stations);
+          return res.json(200, masterDaytime);
+        });
     });
-
 };
