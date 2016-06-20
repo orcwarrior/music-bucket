b55ab052a3b46722672d8f2ca813f72de642a563
orcwarrior <orcwarrior@o2.pl>
2016-06-20 02:28:39 +0200

    soundcloud view: now search for playlist too and uses media-item object.

--- client/app/routes/soundcloud/soundcloud.controller.js
+++ client/app/routes/soundcloud/soundcloud.controller.js
@@ -5,13 +5,18 @@ angular.module('musicBucketApp')
     $scope.message = 'Hello';
     $scope.searchQuery = $location.search().query;
 
-    $scope.search = function(query) {
+    $scope.search = function (query) {
       $location.search('query', query);
       $location.replace();
       soundcloudApi.search.track(query)
-        .then( function (response) {
-          $scope.searchResults = response.data;
+        .then(function (response) {
+          $scope.songSearchResults = response.data;
         });
+      soundcloudApi.search.playlists(query)
+        .then(function (response) {
+          $scope.playlistSearchResults = response.data;
+        });
+
     };
     if ($scope.searchQuery) $scope.search($scope.searchQuery);
   });
