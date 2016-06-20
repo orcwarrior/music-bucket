4ba31096cb41266d70ce5b3e6e8f07244b5f3b33
orcwarrior <orcwarrior@o2.pl>
2016-06-20 02:50:59 +0200

    main.intro (tmp added curSong)

--- client/app/routes/main.intro/main.intro.controller.js
+++ client/app/routes/main.intro/main.intro.controller.js
@@ -1,9 +1,13 @@
 'use strict';
 
 angular.module('musicBucketApp')
-  .controller('MainIntroCtrl', function ($scope, $http) {
+  .controller('MainIntroCtrl', function ($scope, $http, mbPlayerEngine) {
     $scope.windLoc = window.location.host;
 
+    $scope.curSong = function () {
+      var song = mbPlayerEngine.getCurrentSong();
+      return song && song.metainfos && song.metainfos.getSongDescription();
+    }
     $http.get('/api/songzaLurker/stats')
       .then(function (response) {
         $scope.songzaLurkerStats = response.data;
