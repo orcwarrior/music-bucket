2e6656fa0ef18156919a8d264f6386eae963c5d7
orcwarrior <orcwarrior@o2.pl>
2016-06-20 02:05:57 +0200

    mbPlayerList: Switched to virtual scroller from heavy ng-scrollbar playlist:update no longer rebuilds whole listProvider, just updates entries.

--- client/app/mbPlayerList/mbPlayerList.directive.js
+++ client/app/mbPlayerList/mbPlayerList.directive.js
@@ -9,18 +9,12 @@ angular.module('musicBucketApp')
         scope.manager = mbPlayerListManager;
         scope.manager.registerSwapperElement(element.find('.player-list-swapper'));
         scope.manager.registerListScope(scope);
-        var scrollInited = false;
-        scope.initPlaylistScroll = function () {
-          if (!scrollInited) {
-            scope.$broadcast('list-scroll:update', null);
-            scrollInited = true;
-          }
-          scope.playlistScroll = true;
-        }
         // bugfix:
-        scope.$on('playlist:update', function() {
-          var playlist = new playlistListProvider(mbPlayerEngine.getPlaylist());
-          mbPlayerListManager.swapLists(undefined, playlist);
+        scope.$on('playlist:update', function (event, playlist) {
+          if (!(scope.manager.activeList instanceof playlistListProvider)) return;
+          scope.manager.activeList.container.entryRef = playlist;
+          scope.manager.activeList.container.entries = playlist.entries;
+          scope.manager.activeList.container.entriesVals = _.values(playlist.entries);
         });
       }
     };
