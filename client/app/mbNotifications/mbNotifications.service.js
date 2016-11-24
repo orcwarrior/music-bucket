0225983bd8f343f7a2860ef029c756ec243285cd
orcwarrior <orcwarrior@o2.pl>
2016-11-24 23:49:46 +0100

    mbNotifications: tempalte fix, notif. parent El fix, scope fix.

--- client/app/mbNotifications/mbNotifications.service.js
+++ client/app/mbNotifications/mbNotifications.service.js
@@ -1,14 +1,22 @@
 'use strict';
 angular.module('musicBucketApp')
-  .service('mbNotifications', function ($mdToast) {
+  .service('mbNotifications', function ($mdToast, $rootScope) {
+    var parentEl;
     var notifyService = {
       notify: function (template, scope, hideDelay, pos) {
-        $mdToast.show({ template: template,
-        autoWrap: !!template,
-        scope: scope,
-        position: pos || 'bottom right'
+        if (!parentEl) parentEl = (document.getElementsByClassName('mb-body') || [])[0];
+        $mdToast.show({
+          template: '<md-toast>' +
+                    '<div class="md-toast-content">' +
+                    template +
+                    '</div>' +
+                    '</md-toast>',
+          autoWrap: !!template,
+          scope: scope || $rootScope.$new(true),
+          position: pos || 'bottom right',
+          parent: parentEl
         })
       }
-    }
+    };
     return notifyService;
   })
