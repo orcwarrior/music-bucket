2556d8d4c330f80265ae4021eecc664f2d388d52
orcwarrior <orcwarrior@o2.pl>
2017-05-01 04:31:50 +0200

    playlist.ctrl (backend): escaping/unescaping restricted mongo keys in strings

--- server/api/playlist/playlist.controller.js
+++ server/api/playlist/playlist.controller.js
@@ -3,18 +3,62 @@
 var _ = require('lodash');
 var Playlist = require('./playlist.model');
 
+function _escapeMongoKey(id) {
+  if (!_.isString(id)) return id;
+  return id.replace(new RegExp('\\.', 'g'), ':e:')
+    .replace(new RegExp('\\$', 'g'), ':d:');
+}
+function _unescapeMongoKey(id) {
+  if (!_.isString(id)) return id;
+  return id.replace(new RegExp(':e:', 'g'), '\.')
+    .replace(new RegExp(':d:', 'g'), '\$');
+}
+
+function _fixPlaylist(playlist) {
+  var fixedPlaylistEntries = {};
+  _.each(playlist.entries, function (entry, entryKey) {
+    var fixedEntryEntries = _.clone(entry);
+    fixedEntryEntries.entries = {};
+    _.each(entry.entries, function (song, songId) {
+      fixedEntryEntries.entries[_escapeMongoKey(songId)] = song;
+    });
+    fixedPlaylistEntries[_escapeMongoKey(entryKey)] = fixedEntryEntries;
+  });
+  playlist.entries = fixedPlaylistEntries;
+  return playlist;
+}
+function _restorePlaylist(playlist) {
+  var fixedPlaylistEntries = {};
+  _.each(playlist.entries, function (entry, entryKey) {
+    var fixedEntryEntries = _.clone(entry);
+    fixedEntryEntries.entries = {};
+    _.each(entry.entries, function (song, songId) {
+      fixedEntryEntries.entries[_unescapeMongoKey(songId)] = song;
+    });
+    fixedPlaylistEntries[_unescapeMongoKey(entryKey)] = fixedEntryEntries;
+  });
+  playlist.entries = fixedPlaylistEntries;
+  return playlist;
+}
 // Get list of playlists
 exports.index = function (req, res) {
   var uId = (_.isUndefined(req.user)) ? null : req.user._id;
   Playlist.find({
     $or: [{visibility: {$in: ["public", null]}},
       {author: uId, visibility: "private"}]
-  }, function (err, playlists) {
-    if (err) {
-      return handleError(res, err);
-    }
-    return res.json(200, playlists);
-  });
+  })
+    .lean()
+    .exec(function (err, playlists) {
+      if (err) {
+        return handleError(res, err);
+      }
+      //console.log(_.last(playlists));
+      var restoredPlaylists = _.map(playlists, function (p) {
+        return _restorePlaylist(p);
+      });
+      console.log(_.last(restoredPlaylists));
+      return res.json(200, restoredPlaylists);
+    });
 };
 
 // Get a single playlist
@@ -26,17 +70,18 @@ exports.show = function (req, res) {
     if (!playlist) {
       return res.send(404);
     }
-    return res.json(playlist);
+    return res.json(_restorePlaylist(playlist));
   });
 };
 
 // Creates a new playlist in the DB.
 exports.create = function (req, res) {
-  Playlist.create(req.body, function (err, playlist) {
+  Playlist.create(_fixPlaylist(req.body), function (err, playlist) {
     if (err) {
+      console.error(err);
       return handleError(res, err);
     }
-    return res.json(201, playlist);
+    return res.json(201, _restorePlaylist(playlist));
   });
 };
 
@@ -68,11 +113,11 @@ exports.update = function (req, res) {
       if (err) {
         return handleError(res, err);
       }
-      Playlist.create(updated, function (err, playlist) {
+      Playlist.create(_fixPlaylist(updated), function (err, playlist) {
         if (err) {
           return handleError(res, err);
         }
-        return res.json(200, playlist);
+        return res.json(200, _restorePlaylist(playlist));
       });
     });
   });
@@ -111,7 +156,7 @@ exports.advanceTimer = function (req, res) {
     }
 
     playlist.totalPlaytime++;
-    playlist.save(function(err) {
+    playlist.save(function (err) {
       if (err) return handleError(res, err);
       res.send(200);
     });
