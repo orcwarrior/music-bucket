fcbc31dc20b5e29c91c7e2ce4707f02351041faf
orcwarrior <orcwarrior@o2.pl>
2017-10-19 19:49:57 +0200

    route.search: Added query as getParam

--- client/app/routes/search/search.js
+++ client/app/routes/search/search.js
@@ -4,7 +4,7 @@ angular.module('musicBucketApp')
   .config(function ($stateProvider) {
     $stateProvider
       .state('main.search', {
-        url: '/search',
+        url: '/search?query',
         templateUrl: 'app/routes/search/search.html',
         controller : 'SearchCtrl'
       })
